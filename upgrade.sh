421dfeabaa4295aae6c97b830f0166925807bc10
daeks <daeks@gmx.net>
2013-09-25 15:43:36 +0200

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -4,7 +4,7 @@ cd $1
 git checkout upgrade.sh
 git checkout demo/createinstance.sh
 
-git pull https://github.com/CodiadDemo/CodiadDemo.git
+git pull https://github.com/Codiad/CodiadDemo.git
 chown $2:$2 -R $1
 chmod 744 demo/createinstance.sh
 chmod 744 upgrade.sh
