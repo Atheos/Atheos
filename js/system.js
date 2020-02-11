365f1b0a1806a798aaccd05a299286320988d4c3
Liam Siira <liam@siira.us>
2020-02-11 14:02:08 -0500

    Push loadscript to helpers function, maintained references just in case I missed one

--- js/system.js
+++ js/system.js
@@ -15,40 +15,8 @@
 	//////////////////////////////////////////////////////////////////////
 
 	$.loadScript = function(url, arg1, arg2) {
-		// console.trace('Custom LoadScripts');
-		var cache = true,
-			callback = null;
-		//arg1 and arg2 can be interchangable
-		if ($.isFunction(arg1)) {
-			callback = arg1;
-			cache = arg2 || cache;
-		} else {
-			cache = arg1 || cache;
-			callback = arg2 || callback;
-		}
-
-		var load = true;
-		//check all existing script tags in the page for the url
-		jQuery('script[type="text/javascript"]')
-			.each(function() {
-				return load = (url != $(this)
-					.attr('src'));
-			});
-		if (load) {
-			//didn't find it in the page, so load it
-			jQuery.ajax({
-				type: 'GET',
-				url: url,
-				success: callback,
-				dataType: 'script',
-				cache: cache
-			});
-		} else {
-			//already loaded so just call the callback
-			if (jQuery.isFunction(callback)) {
-				callback.call(this);
-			}
-		}
+		console.trace('Custom LoadScript');
+		core.helpers.loadScript(url,arg1,arg2);
 	};
 
 	//////////////////////////////////////////////////////////////////////
