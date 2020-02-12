9c68ff6b45ac8665351c89034fa38a0220f870c2
Liam Siira <liam@siira.us>
2020-02-11 20:00:03 -0500

    Further integrating new confirm dialog window

--- components/project/init.js
+++ components/project/init.js
@@ -6,7 +6,9 @@
 
 (function(global, $) {
 
-	var codiad = global.codiad;
+	var codiad = global.codiad,
+		amplify = global.amplify,
+		i18n = global.i18n;
 
 	$(function() {
 		codiad.project.init();
@@ -131,7 +133,7 @@
 
 		create: function(close) {
 			var _this = this;
-			create = true;
+			// create = true;
 			codiad.modal.load(500, this.dialog + '?action=create&close=' + close);
 			$('#modal_content form')
 				.live('submit', function(e) {
@@ -145,22 +147,31 @@
 						gitBranch = $('#modal_content form input[name="git_branch"]')
 						.val();
 					if (projectPath.indexOf('/') === 0) {
-						create = confirm('Do you really want to create project with absolute path "' + projectPath + '"?');
-					}
-					if (create) {
-						$.get(_this.controller + '?action=create&project_name=' + encodeURIComponent(projectName) + '&project_path=' + encodeURIComponent(projectPath) + '&git_repo=' + gitRepo + '&git_branch=' + gitBranch, function(data) {
-							createResponse = codiad.jsend.parse(data);
-							if (createResponse != 'error') {
-								_this.open(createResponse.path);
-								codiad.modal.unload();
-								_this.loadSide();
-								/* Notify listeners. */
-								amplify.publish('project.onCreate', {
-									"name": projectName,
-									"path": projectPath,
-									"git_repo": gitRepo,
-									"git_branch": gitBranch
-								});
+						codiad.confirm.showConfirm({
+							message: 'Do you really want to create project with absolute path "' + projectPath + '"?',
+							confirm: {
+								message: "Yes",
+								fnc: function() {
+									$.get(_this.controller + '?action=create&project_name=' + encodeURIComponent(projectName) + '&project_path=' + encodeURIComponent(projectPath) + '&git_repo=' + gitRepo + '&git_branch=' + gitBranch, function(data) {
+										var createResponse = codiad.jsend.parse(data);
+										if (createResponse != 'error') {
+											_this.open(createResponse.path);
+											codiad.modal.unload();
+											_this.loadSide();
+											/* Notify listeners. */
+											amplify.publish('project.onCreate', {
+												"name": projectName,
+												"path": projectPath,
+												"git_repo": gitRepo,
+												"git_branch": gitBranch
+											});
+										}
+									});
+								}
+							},
+							deny: {
+								message: "No",
+								fnc: function() {}
 							}
 						});
 					}
@@ -182,7 +193,7 @@
 					var projectName = $('#modal_content form input[name="project_name"]')
 						.val();
 					$.get(_this.controller + '?action=rename&project_path=' + encodeURIComponent(projectPath) + '&project_name=' + encodeURIComponent(projectName), function(data) {
-						renameResponse = codiad.jsend.parse(data);
+						var renameResponse = codiad.jsend.parse(data);
 						if (renameResponse != 'error') {
 							codiad.toast.success(i18n('Project renamed'));
 							_this.loadSide();
@@ -222,7 +233,7 @@
 					}
 					$.get(codiad.filemanager.controller + action, function(d) {
 						$.get(_this.controller + '?action=delete&project_path=' + encodeURIComponent(projectPath), function(data) {
-							deleteResponse = codiad.jsend.parse(data);
+							var deleteResponse = codiad.jsend.parse(data);
 							if (deleteResponse != 'error') {
 								codiad.toast.success(i18n('Project Deleted'));
 								_this.list();
