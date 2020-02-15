0828a7ca88db81ad7aae29cc436d8ec240c1f613
Liam Siira <liam@siira.io>
2020-02-15 13:25:27 -0500

    I changed the intervals use din Atheos to use the new Chronometer module, as well as fully went through the front end and removed all/most Codiad references. Almost officially Atheos!

--- components/keybind/init.js
+++ components/keybind/init.js
@@ -12,65 +12,65 @@
 //////////////////////////////////////////////////////////////////////////////80
 (function(global) {
 
-	var core = global.codiad,
+	var atheos = global.atheos,
 		amplify = global.amplify;
 		
-	amplify.subscribe('core.loaded', function(settings) {
-		core.keybind.init();
+	amplify.subscribe('atheos.loaded', function() {
+		atheos.keybind.init();
 	});
 	//////////////////////////////////////////////////////////////////////
 	// Bindings
 	//////////////////////////////////////////////////////////////////////
-
-	core.keybind = {
+	
+	atheos.keybind = {
 
 		init: function() {
 
 			// Close Modals //////////////////////////////////////////////
 			document.addEventListener('keyup', function(e) {
 				if (e.keyCode === 27) {
-					core.modal.unload();
+					atheos.modal.unload();
 				}
 			});
 
 			// Save [CTRL+S] /////////////////////////////////////////////
 			this.bind(83, function() {
-				core.active.save();
+				atheos.active.save();
 			});
 
 			// Open in browser [CTRL+O] //////////////////////////////////
 			this.bind(79, function() {
-				core.active.openInBrowser();
+				atheos.active.openInBrowser();
 			});
 
 			// Find [CTRL+F] /////////////////////////////////////////////
 			this.bind(70, function() { 
-				core.editor.openSearch('find');
+				atheos.editor.openSearch('find');
 			});
 
 			// Replace [CTRL+R] //////////////////////////////////////////
 			this.bind(82, function() {
-				core.editor.openSearch('replace');
+				atheos.editor.openSearch('replace');
 			});
 
 			// Active List Previous [CTRL+UP] ////////////////////////////
 			this.bind(38, function() {
-				core.active.move('up');
+				atheos.active.move('up');
 			});
 
 			// Active List Next [CTRL+DOWN] //////////////////////////////
 			this.bind(40, function() {
-				core.active.move('down');
+				atheos.active.move('down');
 			});
 
 			// Autocomplete [CTRL+SPACE] /////////////////////////////////
 			// $.ctrl(32, function() {
-			//     core.autocomplete.suggest();
+			//     atheos.autocomplete.suggest();
 			// });
 
 			this.bind(71, function() {
-				if (core.finder) {
-					core.finder.expandFinder();
+				if (atheos.finder) {
+					atheos.finder.expandFinder();
 				}
 			});
 		},
@@ -81,6 +81,7 @@
 		bind: function(key, callback, args) {
 			document.addEventListener('keydown', function(e) {
 				if (e.keyCode === key && (e.ctrlKey || e.metaKey)) {
+					args = args || [];
 					if (!(e.ctrlKey && e.altKey)) {
 						e.preventDefault();
 						callback.apply(this, args);
