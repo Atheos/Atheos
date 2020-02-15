7e65d741570eba9ce4441ca6f2396aa3864cb2da
Liam Siira <liam@siira.io>
2020-02-15 13:30:33 -0500

    I moved to a different minimizer. I realized that if I\'m going to have a compression library built into Atheos, if it does both CSS and JS, it can then be used by a plugin as well and so it made sense. I might remove the library from releases later though.

--- public/minify-core-js.php
+++ public/minify-core-js.php
@@ -1,55 +1,95 @@
 <?php
 
-require 'lib/Minifier.php';
+// require 'lib/Minifier.php';
 
-$files = [
-	"js/jquery-1.7.2.min.js",
-	"js/jquery-ui-1.8.23.custom.min.js",
-	"js/amplify.js",
-	"js/localstorage.js",
-	"js/hoverintent.min.js",
-	"js/ajax.js",
-	"js/file-icons.js",
-	"js/onyx.js",
-	"js/system.js",
-	"js/helpers.js",
-	"js/synthetic.js",
-	"js/sidebars.js",
-	"js/confirm.js",
-	"js/modal.js",
-	"js/toast.js",
-	"js/jsend.js",
-	"js/instance.js"
-];
+require_once 'lib/minify/src/Minify.php';
+require_once 'lib/minify/src/CSS.php';
+require_once 'lib/minify/src/JS.php';
+require_once 'lib/minify/src/Exception.php';
+require_once 'lib/minify/src/Exceptions/BasicException.php';
+require_once 'lib/minify/src/Exceptions/FileImportException.php';
+require_once 'lib/minify/src/Exceptions/IOException.php';
+require_once 'lib/path-converter/src/ConverterInterface.php';
+require_once 'lib/path-converter/src/Converter.php';
 
-$minified = 'public/core.min.js';
+use MatthiasMullie\Minify;
 
-if (file_exists($minified)) {
-	$mostRecent = filemtime($minified);
-	foreach ($files as $file) {
-		if (filemtime($file) > $mostRecent) {
-			$mostRecent = filemtime($file);
-			break;
-		}
-	}
-	if (filemtime($minified) < $mostRecent) {
-		$javascript = '';
-		foreach ($files as $file) {
-			$javascript .= file_get_contents($file) . ';' . PHP_EOL;
-		}
-		$minified_javascript = "// Creation Time: " . time() . PHP_EOL;
-		$minified_javascript .= \JShrink\Minifier::minify($javascript);
-		file_put_contents($minified, $minified_javascript);
-	}
+$debug = false;
+if ($debug == true) {
+
+
+	echo("	<script src=\"js/amplify.js\"></script>");
+	echo("	<script src=\"js/hoverintent.min.js\"></script>");
+	echo("	<script src=\"js/ajax.js\"></script>");
+
+	echo("	<script src=\"js/onyx.js\"></script>");
+	echo("	<script src=\"js/file-icons.js\"></script>");
+	echo("	<script src=\"js/synthetic.js\"></script>");
+
+	echo("	<script src=\"js/system.js\"></script>");
+
+	echo("	<script src=\"js/helpers.js\"></script>");
+	echo("	<script src=\"js/storage.js\"></script>");
+	echo("	<script src=\"js/sidebars.js\"></script>");
+	echo("	<script src=\"js/confirm.js\"></script>");
+	echo("	<script src=\"js/modal.js\"></script>");
+	echo("	<script src=\"js/toast.js\"></script>");
+	echo("	<script src=\"js/jsend.js\"></script>");
+
 } else {
+	$files = [
+		"js/jquery-1.7.2.min.js",
+		"js/jquery-ui-1.8.23.custom.min.js",
+		"js/amplify.js",
+		"js/hoverintent.min.js",
+		"js/ajax.js",
+		"js/file-icons.js",
+		"js/onyx.js",
+		"js/synthetic.js",
+		"js/system.js",
+		"js/helpers.js",
+		"js/storage.js",
+		"js/sidebars.js",
+		"js/confirm.js",
+		"js/modal.js",
+		"js/toast.js",
+		"js/jsend.js"
+	];
+
+	$minified = 'public/core.min.js';
+
+	function minifyJS($minified, $files) {
+
+
 		$javascript = '';
-	foreach ($files as $file) {
-		$javascript .= file_get_contents($file) . ';' . PHP_EOL;
-	}
 		$minified_javascript = "// Creation Time: " . time() . PHP_EOL;
-		$minified_javascript .= \JShrink\Minifier::minify($javascript);
+		foreach ($files as $file) {
+			$javascript = file_get_contents($file);
+			$minified_javascript .= "// $file" . PHP_EOL;
+			$minifier = new Minify\JS($javascript);
+			$minified_javascript .= $minifier->minify() . ';' . PHP_EOL;
+			// $minified_javascript .= \JShrink\Minifier::minify($javascript, array('flaggedComments' => false));
 
-	file_put_contents($minified, $minified_javascript);
+		}
+		// $minified_javascript .= \JShrink\Minifier::minify($javascript, array('flaggedComments' => false));
+		file_put_contents($minified, $minified_javascript);
+	};
+
+	// if (file_exists($minified)) {
+	// 	$mostRecent = filemtime($minified);
+	// 	foreach ($files as $file) {
+	// 		if (filemtime($file) > $mostRecent) {
+	// 			$mostRecent = filemtime($file);
+	// 			break;
+	// 		}
+	// 	}
+	// 	if (filemtime($minified) < $mostRecent) {
+	// 		minifyJS($minified, $files);
+	// 	}
+	// } else {
+	minifyJS($minified, $files);
+	// }
+
+	echo("<script src=\"$minified\"></script>");
+
 }
-
-echo("<script src=\"$minified\"></script>");
