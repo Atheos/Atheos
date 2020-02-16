897a5fec6c1bdaf2dae5631fc97d0392f8ec5c60
Liam Siira <liam@siira.us>
2020-02-16 12:15:42 -0500

    This file literally does nothing, but I\'m going to clean it up anyway just in case I decided to start implementing it later.

--- components/worker_manager/init.js
+++ components/worker_manager/init.js
@@ -1,71 +1,82 @@
-(function(global, $){
+//////////////////////////////////////////////////////////////////////////////80
+// Worker Manager
+//////////////////////////////////////////////////////////////////////////////80
+// Copyright (c) Atheos & Liam Siira (Atheos.io), distributed as-is and without
+// warranty under the modified License: MIT - Hippocratic 1.2: firstdonoharm.dev
+// See [root]/license.md for more. This information must remain intact.
+//////////////////////////////////////////////////////////////////////////////80
+// Notes: 
+// This is a module.
+//												- Liam Siira
+//////////////////////////////////////////////////////////////////////////////80
 
-    codiad.workerManager = {
-        taskQueue: [],
-        addTask: function(taskConfig, callback, context){
-            var _this = this;
-            if(_this.worker !== null) {
-                this.taskQueue.push({
-                    config: taskConfig,
-                    callback: callback,
-                    context: context
-                });
+(function(global, $) {
 
-                this.clearSubsidableTasks(taskConfig.id);
+	codiad.workerManager = {
+		taskQueue: [],
+		addTask: function(taskConfig, callback, context) {
+			var _this = this;
+			if (_this.worker !== null) {
+				this.taskQueue.push({
+					config: taskConfig,
+					callback: callback,
+					context: context
+				});
 
-                if (! this.workerRunning()) {
-                    var initStatus = this.initiateWorker();
-                    if (! initStatus) {
-                        callback(null, false);
-                        return;
-                    }
-                    this.worker.addEventListener('message', function(e){
-                        _this.concludeTask(e.data);
-                    }, false);
-                }
+				this.clearSubsidableTasks(taskConfig.id);
 
-                if (this.taskQueue.length == 1) {
-                    this.scheduleNext();
-                }
-            } else {
-                callback(false,taskConfig.id);
-            }
-        },
-        workerRunning: function(){
-            return !! this.worker;
-        },
-        initiateWorker: function(){
-            if ( typeof Worker !== 'undefined' && Worker !== null )
-        	{
-        		this.worker = new Worker('components/worker_manager/worker.js');
-        		return !! this.worker;
-        	}
-        },
-        clearSubsidableTasks: function(id){
-            var i = this.taskQueue.length -2;
-            while(i > 0) {
-                if (this.taskQueue[i].id == id) {
-                    this.taskQueue.splice(i, 1);
-                }
-                i--;
-            }
-        },
-        scheduleNext: function(){
-            var taskConfig = this.taskQueue[0].config;
-            this.worker.postMessage(taskConfig);
-        },
-        concludeTask: function(msg){
-            if (this.taskQueue.length > 0) {
-                var tq = this.taskQueue[0];
-                callback = tq.callback;
-                context = tq.context;
-                this.taskQueue.splice(0, 1);
-                if (this.taskQueue.length > 0) {
-                    this.scheduleNext();
-                }
-                tq.callback.apply(context, [msg.success, msg.result]);
-            }
-        }
-    }
+				if (!this.workerRunning()) {
+					var initStatus = this.initiateWorker();
+					if (!initStatus) {
+						callback(null, false);
+						return;
+					}
+					this.worker.addEventListener('message', function(e) {
+						_this.concludeTask(e.data);
+					}, false);
+				}
+
+				if (this.taskQueue.length == 1) {
+					this.scheduleNext();
+				}
+			} else {
+				callback(false, taskConfig.id);
+			}
+		},
+		workerRunning: function() {
+			return !!this.worker;
+		},
+		initiateWorker: function() {
+			if (typeof Worker !== 'undefined' && Worker !== null) {
+				this.worker = new Worker('components/worker_manager/worker.js');
+				return !!this.worker;
+			}
+		},
+		clearSubsidableTasks: function(id) {
+			var i = this.taskQueue.length - 2;
+			while (i > 0) {
+				if (this.taskQueue[i].id == id) {
+					this.taskQueue.splice(i, 1);
+				}
+				i--;
+			}
+		},
+		scheduleNext: function() {
+			var taskConfig = this.taskQueue[0].config;
+			this.worker.postMessage(taskConfig);
+		},
+		concludeTask: function(msg) {
+			if (this.taskQueue.length > 0) {
+				var tq = this.taskQueue[0];
+				callback = tq.callback;
+				context = tq.context;
+				this.taskQueue.splice(0, 1);
+				if (this.taskQueue.length > 0) {
+					this.scheduleNext();
+				}
+				tq.callback.apply(context, [msg.success, msg.result]);
+			}
+		}
+	}
 
 })(this, jQuery);
