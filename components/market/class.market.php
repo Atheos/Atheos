6e1864e5c55af9cd8c6746eb2568dbbc6c4d54a2
Liam Siira <liam@siira.io>
2020-02-21 13:03:55 -0500

    Stopped the market using FileName from our installation as the plugin name in the market

--- components/market/class.market.php
+++ components/market/class.market.php
@@ -122,7 +122,7 @@ class Market extends Common
                 if (!$found && file_exists(PLUGINS . "/" . $fname . "/plugin.json")) {
                     $data = file_get_contents(PLUGINS . "/" . $fname . "/plugin.json");
                     $data = json_decode($data, true);
-                    $data[0]['name'] = $fname;
+                    // $data[0]['name'] = $fname;
                     $data[0]['folder'] = $fname;
                     $data[0]['type'] = 'plugins';
                     $data[0]['image'] = '';
@@ -152,7 +152,7 @@ class Market extends Common
                 if (!$found && file_exists(THEMES . "/" . $fname . "/theme.json")) {
                     $data = file_get_contents(THEMES . "/" . $fname . "/theme.json");
                     $data = json_decode($data, true);
-                    $data[0]['name'] = $fname;
+                    // $data[0]['name'] = $fname;
                     $data[0]['folder'] = $fname;
                     $data[0]['type'] = 'themes';
                     $data[0]['image'] = '';
