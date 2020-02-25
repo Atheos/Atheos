314c99f58c389e2242ab68565a8aefc854855c0d
Liam Siira <liam@siira.us>
2020-02-25 13:41:06 -0500

    Honestly lost a little bit of what I was working on in a huge rabbit hole. The story was trying to modify the SassCompiler plugin, I hit the settings area and realized that it was a mess, had to fix before I could do much else and kept going until I fixed a ton of small css inconsistencies. I have a lot more work to do on the settings component but it\'s going to be a big push away from compatibility with older plugins.

--- components/filemanager/dialog.php
+++ components/filemanager/dialog.php
@@ -25,7 +25,7 @@ checkSession();
 		//////////////////////////////////////////////////////////////////
 		case 'create':
 			?>
-			<label><span class="icon-pencil"></span><?php echo i18n((ucfirst($_GET['type']))); ?></label>
+			<label><i class="fas fa-plus-circle"></i><?php echo i18n((ucfirst($_GET['type']))); ?></label>
 			<input type="text" name="nodeName" autofocus="autofocus" autocomplete="off">
 			<button class="btn-left"><?php i18n("Create"); ?></button>
 			<button class="btn-right" onclick="codiad.modal.unload(); return false;"><?php i18n("Cancel"); ?></button>
@@ -39,9 +39,26 @@ checkSession();
 			?>
 			<input type="hidden" name="path" value="<?php echo($_GET['path']); ?>">
 			<input type="hidden" name="type" value="<?php echo($_GET['type']); ?>">
-			<label><span class="icon-pencil"></span> <?php i18n("Rename"); ?> <?php echo i18n((ucfirst($_GET['type']))); ?></label>
+			<label><i class="fas fa-pencil-alt"></i> <?php i18n("Rename"); ?> <?php echo i18n((ucfirst($_GET['type']))); ?></label>
 			<input type="text" name="object_name" autofocus="autofocus" autocomplete="off" value="<?php echo($_GET['nodeName']); ?>">
 			<button class="btn-left"><?php i18n("Rename"); ?></button>
+			<button class="btn-right" onclick="codiad.modal.unload(); return false;"><?php i18n("Cancel"); ?></button>
+			<?php
+			break;
+
+		//////////////////////////////////////////////////////////////////
+		// Duplicate
+		//////////////////////////////////////////////////////////////////
+		case 'duplicate':
+			?>
+			<input type="hidden" name="path" value="<?php echo($_GET['path']); ?>">
+			<input type="hidden" name="type" value="<?php echo($_GET['type']); ?>">
+			<label><i class="fa fa-clone"></i> <?php i18n("Duplicate"); ?> <?php echo i18n((ucfirst($_GET['type']))); ?></label>
+			<p>
+				Enter new name
+			</p>
+			<input type="text" name="object_name" autofocus="autofocus" autocomplete="off" value="<?php echo($_GET['nodeName']); ?>">
+			<button class="btn-left"><?php i18n("Duplicate"); ?></button>
 			<button class="btn-right" onclick="codiad.modal.unload(); return false;"><?php i18n("Cancel"); ?></button>
 			<?php
 			break;
@@ -52,7 +69,7 @@ checkSession();
 		case 'delete':
 			?>
 			<input type="hidden" name="path" value="<?php echo($_GET['path']); ?>">
-			<label><?php i18n("Are you sure you wish to delete the following:"); ?></label>
+			<label><i class="fas fa-trash-alt"></i><?php i18n("Are you sure you wish to delete the following:"); ?></label>
 			<pre><?php if (!FileManager::isAbsPath($_GET['path'])) {
 				echo '/';
 			}; echo($_GET['path']); ?></pre>
@@ -66,7 +83,7 @@ checkSession();
 		//////////////////////////////////////////////////////////////////
 		case 'preview':
 			?>
-			<label><?php i18n("Inline Preview"); ?></label>
+			<label><i class="fas fa-eye"></i><?php i18n("Inline Preview"); ?></label>
 			<div>
 				<br><br><img src="<?php echo(str_replace(BASE_PATH . "/", "", WORKSPACE) . "/" . $_GET['path']); ?>"><br><br>
 			</div>
@@ -119,7 +136,7 @@ checkSession();
 					</td>
 				</tr>
 				<tr>
-					<td colspan="3">
+					<td coli="3">
 						<label><?php i18n("File Type:"); ?></label>
 						<input type="text" name="search_file_type" placeholder="<?php i18n("space seperated file types eg: js c php"); ?>">
 					</td>
--- components/settings/init.js
+++ components/settings/init.js
@@ -1,19 +1,20 @@
 /*
-	*  Copyright (c) Codiad, distributed
+	*  Copyright (c) atheos, distributed
 	*  as-is and without warranty under the MIT License. See
 	*  [root]/license.txt for more. This information must remain intact.
 	*/
 
 (function(global, $) {
-	var codiad = global.codiad,
-		amplify = global.amplify;
+	var atheos = global.atheos,
+		amplify = global.amplify,
+		o = global.onyx;
 
 
-	$(function() {
-		codiad.settings.init();
+	amplify.subscribe('atheos.loaded', function(settings) {
+		atheos.settings.init();
 	});
 
-	codiad.settings = {
+	atheos.settings = {
 
 		controller: 'components/settings/controller.php',
 
@@ -27,26 +28,80 @@
 				*/
 			$('body').append('<iframe src="components/settings/dialog.php?action=iframe"></iframe>');
 			$('#settings').on('click', function() {
-				codiad.settings.show();
+				atheos.settings.show();
 			});
 			this.load();
 
 		},
 
+		liveChange: function(target) {
+
+			var setting = target.attr('data-setting');
+
+			var value = target.value();
+			var boolean = (value === 'true');
+			var int = (!isNaN(parseFloat(value)) && isFinite(value)) ? parseInt(value, 10) : 0;
+
+			console.log(setting);
+
+			if (value === null) {
+				atheos.toast.alert("You Must Choose A Value");
+				return;
+			} else {
+				switch (setting) {
+					case 'atheos.editor.theme':
+						atheos.editor.setTheme(value);
+						break;
+					case 'atheos.editor.fontSize':
+						atheos.editor.setFontSize(value);
+						break;
+					case 'atheos.editor.highlightLine':
+						atheos.editor.setHighlightLine(value);
+						break;
+					case 'atheos.editor.indentGuides':
+						atheos.editor.setIndentGuides(boolean);
+						break;
+					case 'atheos.editor.printMargin':
+						atheos.editor.setPrintMargin(boolean);
+						break;
+					case 'atheos.editor.printMarginColumn':
+						atheos.editor.setPrintMarginColumn(int);
+						break;
+					case 'atheos.editor.wrapMode':
+						atheos.editor.setWrapMode(boolean);
+						break;
+					case 'atheos.editor.rightSidebarTrigger':
+						atheos.editor.setRightSidebarTrigger(boolean);
+						break;
+					case 'atheos.editor.fileManagerTrigger':
+						atheos.editor.setFileManagerTrigger(boolean);
+						break;
+					case 'atheos.editor.persistentModal':
+						atheos.editor.setPersistentModal(boolean);
+						break;
+					case "atheos.editor.softTabs":
+						atheos.editor.setSoftTabs(boolean);
+						break;
+					case "atheos.editor.tabSize":
+						atheos.editor.setTabSize(value);
+						break;
+				}
+			}
+		},
+
 		//////////////////////////////////////////////////////////////////
 		// Save Settings
 		//////////////////////////////////////////////////////////////////
-
-		save: function() {
+		save: function(notify) {
 			var key, settings = {};
-			var systemRegex = /^codiad/;
-			var pluginRegex = /^codiad.plugin/;
+			var systemRegex = /^atheos/;
+			var pluginRegex = /^atheos.plugin/;
 
-			/* Notify listeners */
-			amplify.publish('settings.save', {});
 
-			var syncSystem = (localStorage.getItem('codiad.settings.system.sync') === "true");
-			var syncPlugin = (localStorage.getItem('codiad.settings.plugin.sync') === "true");
+			var syncSystem = (localStorage.getItem('atheos.settings.system.sync') === "true");
+			var syncPlugin = (localStorage.getItem('atheos.settings.plugin.sync') === "true");
+			
+			var panel = o('#settings #panel_view');
 
 			if (syncSystem || syncPlugin) {
 				for (var i = 0; i < localStorage.length; i++) {
@@ -60,14 +115,57 @@
 				}
 			}
 
-			settings['codiad.settings.system.sync'] = syncSystem;
-			settings['codiad.settings.plugin.sync'] = syncPlugin;
+			settings['atheos.settings.system.sync'] = syncSystem;
+			settings['atheos.settings.plugin.sync'] = syncPlugin;
 
 			$.post(this.controller + '?action=save', {
 				settings: JSON.stringify(settings)
 			}, function(data) {
-				parsed = codiad.jsend.parse(data);
+				parsed = atheos.jsend.parse(data);
 			});
+
+			/* Notify listeners */
+			if (notify) {
+				amplify.publish('settings.save');
+			}
+
+		},
+		
+		saveOld: function(notify) {
+			var key, settings = {};
+			var systemRegex = /^atheos/;
+			var pluginRegex = /^atheos.plugin/;
+
+
+			var syncSystem = (localStorage.getItem('atheos.settings.system.sync') === "true");
+			var syncPlugin = (localStorage.getItem('atheos.settings.plugin.sync') === "true");
+
+			if (syncSystem || syncPlugin) {
+				for (var i = 0; i < localStorage.length; i++) {
+					key = localStorage.key(i);
+					if (systemRegex.test(key) && !pluginRegex.test(key) && syncSystem) {
+						settings[key] = localStorage.getItem(key);
+					}
+					if (pluginRegex.test(key) && syncPlugin) {
+						settings[key] = localStorage.getItem(key);
+					}
+				}
+			}
+
+			settings['atheos.settings.system.sync'] = syncSystem;
+			settings['atheos.settings.plugin.sync'] = syncPlugin;
+
+			$.post(this.controller + '?action=save', {
+				settings: JSON.stringify(settings)
+			}, function(data) {
+				parsed = atheos.jsend.parse(data);
+			});
+
+			/* Notify listeners */
+			if (notify) {
+				amplify.publish('settings.save');
+			}
+
 		},
 
 		//////////////////////////////////////////////////////////////////
@@ -76,7 +174,7 @@
 
 		load: function() {
 			$.get(this.controller + '?action=load', function(data) {
-				var parsed = codiad.jsend.parse(data);
+				var parsed = atheos.jsend.parse(data);
 				if (parsed !== 'error') {
 					$.each(parsed, function(i, item) {
 						localStorage.setItem(i, item);
@@ -97,14 +195,37 @@
 		//////////////////////////////////////////////////////////////////
 
 		show: function(dataFile) {
-			var _this = this;
-			codiad.modal.load(800, 'components/settings/dialog.php?action=settings');
-			codiad.modal.hideOverlay();
-			codiad.modal.ready.then(function() {
+			var settings = this;
+			atheos.modal.load(800, 'components/settings/dialog.php?action=settings');
+
+			atheos.modal.ready.then(function() {
+
+				o('#modal_wrapper').on('change', function(e) {
+					var target = o(e.target);
+					var tagName = target.el.tagName;
+					var type = target.el.type;
+					if (tagName === 'SELECT' || (tagName === 'INPUT' && type === 'checkbox')) {
+						settings.liveChange(target);
+						settings.save(false);
+					}
+				});
+
+				o('#panel_menu').on('click', function(e) {
+					var target = o(e.target);
+					var tagName = target.el.tagName;
+					if (tagName === 'A') {
+						settings.showTab(target.attr('data-file'));
+					}
+				});
+
+
+				$('.settings-view .config-menu li').click(function() {});
+
+
 				if (typeof(dataFile) == 'string') {
-					codiad.settings._showTab(dataFile);
+					settings.showTab(dataFile);
 				} else {
-					_this._loadTabValues('components/settings/settings.editor.php');
+					settings.loadTabValues('components/settings/settings.editor.php');
 				}
 				/* Notify listeners */
 				amplify.publish('settings.dialog.show', {});
@@ -121,26 +242,27 @@
 		//
 		//////////////////////////////////////////////////////////////////
 
-		_showTab: function(dataFile) {
-			var _this = this;
-			if (typeof(dataFile) != 'string') {
-				return false;
-			}
-			$('.settings-view .config-menu .active').removeClass('active');
-			$('.settings-view .config-menu li[data-file="' + dataFile + '"]').addClass('active');
-			$('.settings-view .panels .active').hide().removeClass('active');
-			//Load panel
-			if ($('.settings-view .panel[data-file="' + dataFile + '"]').length === 0) {
-				$('.settings-view .panels').append('<div class="panel active" data-file="' + dataFile + '"></div>');
-				$('.settings-view .panel[data-file="' + dataFile + '"]').load(dataFile, function() {
+		showTab: function(dataFile) {
+			var settings = this;
+			if (typeof(dataFile) === 'string') {
+
+				settings.save(false);
+
+				o('#panel_menu .active').removeClass('active');
+				o('#panel_menu a[data-file="' + dataFile + '"]').addClass('active');
+
+				o('#panel_view').empty();
+
+				//Load panel
+				// $('#settings panel_view').append('<div class="panel active" data-file="' + dataFile + '"></div>');
+				$('#panel_view').load(dataFile, function() {
 					//TODO Show and hide loading information
 					/* Notify listeners */
 					var name = $('.settings-view .config-menu li[data-file="' + dataFile + '"]').attr('data-name');
 					amplify.publish('settings.dialog.tab_loaded', name);
-					_this._loadTabValues(dataFile);
+					settings.loadTabValues(dataFile);
 				});
-			} else {
-				$('.settings-view .panel[data-file="' + dataFile + '"]').show().addClass('active');
+
 			}
 		},
 
@@ -153,7 +275,7 @@
 		//  dataFile - {String} - Location of settings file based on BASE_URL
 		//
 		//////////////////////////////////////////////////////////////////
-		_loadTabValues: function(dataFile) {
+		loadTabValues: function(dataFile) {
 			//Load settings
 			var key, value;
 			$('.settings-view .panel[data-file="' + dataFile + '"] .setting').each(function(i, item) {
