21d086d0c00c4aaac03275a8db22edae95e73d12
Liam Siira <liam@siira.io>
2020-03-04 18:48:46 -0500

    Removed repeatingly repetitive repeats from the settings menus that repeated themselves.
    Former-commit-id: efc7111704a7ef0d43441491444f46d2b190a3fe

--- components/settings/settings.system.php
+++ components/settings/settings.system.php
@@ -2,9 +2,18 @@
 <h2><i class="fas fa-sliders-h"></i><?php i18n("System Settings"); ?></h2>
 <table class="settings">
 	<tr>
+		<td><?php i18n("Left Sidebar Trigger"); ?></td>
+		<td>
+			<select class="setting" data-setting="sidebars.leftOpenOnClick">
+				<option value="false" default><?php i18n("Hover") ?></option>
+				<option value="true"><?php i18n("Click") ?></option>
+			</select>
+		</td>
+	</tr>	
+	<tr>
 		<td><?php i18n("Right Sidebar Trigger"); ?></td>
 		<td>
-			<select class="setting" data-setting="atheos.editor.rightSidebarTrigger">
+			<select class="setting" data-setting="sidebars.rightOpenOnClick">
 				<option value="false" default><?php i18n("Hover") ?></option>
 				<option value="true"><?php i18n("Click") ?></option>
 			</select>
@@ -13,7 +22,7 @@
 	<tr>
 		<td><?php i18n("Filemanager Trigger"); ?></td>
 		<td>
-			<select class="setting" data-setting="atheos.editor.fileManagerTrigger">
+			<select class="setting" data-setting="editor.fileManagerTrigger">
 				<option value="false" default><?php i18n("Single Click") ?></option>
 				<option value="true"><?php i18n("Double Click") ?></option>
 			</select>
@@ -23,7 +32,7 @@
 	<tr>
 		<td><?php i18n("Sync system settings on all devices"); ?></td>
 		<td>
-			<select class="setting" data-setting="atheos.settings.system.sync">
+			<select class="setting" data-setting="settings.syncSystem">
 				<option value="true"><?php i18n("Yes") ?></option>
 				<option value="false" default><?php i18n("No") ?></option>
 			</select>
@@ -33,7 +42,7 @@
 	<tr>
 		<td><?php i18n("Sync plugin settings on all devices"); ?></td>
 		<td>
-			<select class="setting" data-setting="atheos.settings.plugin.sync">
+			<select class="setting" data-setting="settings.syncPlugin">
 				<option value="true"><?php i18n("Yes") ?></option>
 				<option value="false" default><?php i18n("No") ?></option>
 			</select>
