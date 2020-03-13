26ecd3692a326086f1425afecba21d62c9e85120
Liam Siira <liam@siira.us>
2020-03-13 11:21:01 -0400

    Created a new component to handle content searching files\; while it makes sense to combine both the finder and the new scout component into one, they both achieve distinct goals using similar server side functions, so I\'m still on the fence about it. In doing so, I removed all the now-scout code from the file manager and tried to put everything where it was supposed to be.

---
+++ components/scout/dialog.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+*  Copyright (c) atheos & Kent Safranski (atheos.com), distributed
+*  as-is and without warranty under the MIT License. See
+*  [root]/license.txt for more. This information must remain intact.
+*/
+
+require_once('../../common.php');
+
+//////////////////////////////////////////////////////////////////
+// Verify Session or Key
+//////////////////////////////////////////////////////////////////
+
+checkSession();
+
+?>
+<form onsubmit="return;">
+	<?php
+
+	switch ($_GET['action']) {
+
+		//////////////////////////////////////////////////////////////////
+		// Search
+		//////////////////////////////////////////////////////////////////
+		case 'search':
+			?>
+			<input type="hidden" name="path" value="<?php echo($_GET['path']); ?>">
+			<table class="file-search-table">
+				<tr>
+					<td width="65%">
+						<label><?php i18n("Search Files:"); ?></label>
+						<input type="text" name="search_string" autofocus="autofocus">
+					</td>
+					<td width="5%">&nbsp;&nbsp;</td>
+					<td>
+						<label><?php i18n("In:"); ?></label>
+						<select name="search_type">
+							<option value="0"><?php i18n("Current Project"); ?></option>
+							<?php if (checkAccess()) {
+								?>
+								<option value="1"><?php i18n("Workspace Projects"); ?></option>
+								<?php
+							} ?>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td coli="3">
+						<label><?php i18n("File Type:"); ?></label>
+						<input type="text" name="search_file_type" placeholder="<?php i18n("space seperated file types eg: js c php"); ?>">
+					</td>
+				</tr>
+			</table>
+			<pre id="search_results"></pre>
+			<div id="search_processing"></div>
+			<button class="btn-left"><?php i18n("Search"); ?></button>
+			<button class="btn-right" onclick="atheos.modal.unload();return false;"><?php i18n("Cancel"); ?></button>
+			<?php
+			break;
+
+	}
+
+	?>
+</form>
