07eb9fb028be8c987350cd2e4bd41e3b898ef3df
Liam Siira <liam@siira.io>
2020-03-15 12:18:08 -0400

    Copied some functions from elsewhere into here, need to look into and see which ones work best
    Former-commit-id: 3b4b09932f8dd1860c63a8c530ecb17f791f84a1

--- modules/common.js
+++ modules/common.js
@@ -58,6 +58,30 @@ var log = function(m, t) {
 		getNodeType: function(path) {
 			return o('#file-manager a[data-path="' + path + '"]').attr('data-type');
 		},
+		
+		_basename: function(path, suffix) {
+			//  discuss at: http://phpjs.org/functions/basename/
+			var b = path;
+			var lastChar = b.charAt(b.length - 1);
+
+			if (lastChar === '/' || lastChar === '\\') {
+				b = b.slice(0, -1);
+			}
+
+			b = b.replace(/^.*[\/\\]/g, '');
+
+			if (typeof suffix === 'string' && b.substr(b.length - suffix.length) == suffix) {
+				b = b.substr(0, b.length - suffix.length);
+			}
+
+			return b;
+		},
+
+		_dirname: function(path) {
+			// discuss at: http://phpjs.org/functions/dirname/
+			return path.replace(/\\/g, '/')
+				.replace(/\/[^\/]*\/?$/, '');
+		},
 
 		//////////////////////////////////////////////////////////////////////
 		// Extend
