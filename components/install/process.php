06e5f14a0f97963dd4e0307cdff8b9d3eeee844f
Liam Siira <liam@siira.io>
2020-03-17 23:52:04 -0400

    Rewrote the user backend and frontend to user the more standardized / perfected structure that I\'m starting to use throughout Atheos. I also changed the way projects, and permissions worked. Now, there will no longer be an ACL file per suer, but instead their ACL will be built into there user JSON data, and there is a new permissions setting to actually test user permissions. Before, you were an admin if you didn\'t have an ACL and that just seems a little extreme. I\'m also hoping that I can integrate this further to allow read only users. Maybe even take it so far to have permissions by project, but that might be a little extreme.

--- components/install/process.php
+++ components/install/process.php
@@ -10,61 +10,50 @@
 // Paths
 //////////////////////////////////////////////////////////////////////
 
-    $path = $_POST['path'];
+$path = $_POST['path'];
 
-    $rel = str_replace('/components/install/process.php', '', $_SERVER['REQUEST_URI']);
+$rel = str_replace('/components/install/process.php', '', $_SERVER['REQUEST_URI']);
 
-    $workspace = $path . "/workspace";
-    $users = $path . "/data/users.php";
-    $projects = $path . "/data/projects.php";
-    $active = $path . "/data/active.php";
-    $config = $path . "/config.php";
+$workspace = $path . "/workspace";
+$users = $path . "/data/users.json";
+$projects = $path . "/data/projects.json";
+$active = $path . "/data/active.json";
+$config = $path . "/config.php";
 
 //////////////////////////////////////////////////////////////////////
 // Functions
 //////////////////////////////////////////////////////////////////////
 
-function saveFile($file, $data)
-{
-    $write = fopen($file, 'w') or die("can't open file");
-    fwrite($write, $data);
-    fclose($write);
+function saveFile($file, $data) {
+	$write = fopen($file, 'w') or die("can't open file");
+	fwrite($write, $data);
+	fclose($write);
 }
 
-function saveJSON($file, $data)
-{
-    $data = "<?php/*|\r\n" . json_encode($data) . "\r\n|*/?>";
-    saveFile($file, $data);
+function saveJSON($file, $data) {
+	$data = json_encode($data, JSON_PRETTY_PRINT);
+	saveFile($file, $data);
 }
 
-function encryptPassword($p)
-{
-    // return sha1(md5($p));
-    return password_hash($p, PASSWORD_DEFAULT);
+function cleanUsername($username) {
+	return preg_replace('#[^A-Za-z0-9'.preg_quote('-_@. ').']#', '', $username);
 }
 
-function cleanUsername($username)
-{
-    return preg_replace('#[^A-Za-z0-9'.preg_quote('-_@. ').']#', '', $username);
+function isAbsPath($path) {
+	return $path[0] === '/';
 }
 
-function isAbsPath($path)
-{
-    return $path[0] === '/';
-}
+function cleanPath($path) {
 
-function cleanPath($path)
-{
+	// prevent Poison Null Byte injections
+	$path = str_replace(chr(0), '', $path);
 
-    // prevent Poison Null Byte injections
-    $path = str_replace(chr(0), '', $path);
+	// prevent go out of the workspace
+	while (strpos($path, '../') !== false) {
+		$path = str_replace('../', '', $path);
+	}
 
-    // prevent go out of the workspace
-    while (strpos($path, '../') !== false) {
-        $path = str_replace('../', '', $path);
-    }
-
-    return $path;
+	return $path;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -72,68 +61,74 @@ function cleanPath($path)
 //////////////////////////////////////////////////////////////////////
 
 if (!file_exists($users) && !file_exists($projects) && !file_exists($active)) {
-    //////////////////////////////////////////////////////////////////
-    // Get POST responses
-    //////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////
+	// Get POST responses
+	//////////////////////////////////////////////////////////////////
 
-    $username = cleanUsername($_POST['username']);
-    $password = encryptPassword($_POST['password']);
-    $project_name = $_POST['project_name'];
-    if (isset($_POST['project_path'])) {
-        $project_path = $_POST['project_path'];
-    } else {
-        $project_path = $project_name;
-    }
-    $timezone = $_POST['timezone'];
+	$username = cleanUsername($_POST['username']);
+	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+	$project_name = $_POST['project_name'];
+	if (isset($_POST['project_path'])) {
+		$project_path = $_POST['project_path'];
+	} else {
+		$project_path = $project_name;
+	}
+	$timezone = $_POST['timezone'];
 
-    //////////////////////////////////////////////////////////////////
-    // Create Projects files
-    //////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////
+	// Create Projects files
+	//////////////////////////////////////////////////////////////////
 
-    $project_path = cleanPath($project_path);
+	$project_path = cleanPath($project_path);
 
-    if (!isAbsPath($project_path)) {
-        $project_path = str_replace(" ", "_", preg_replace('/[^\w-\.]/', '', $project_path));
-        mkdir($workspace . "/" . $project_path);
-    } else {
-        $project_path = cleanPath($project_path);
-        if (substr($project_path, -1) == '/') {
-            $project_path = substr($project_path, 0, strlen($project_path)-1);
-        }
-        if (!file_exists($project_path)) {
-            if (!mkdir($project_path.'/', 0755, true)) {
-                die("Unable to create Absolute Path");
-            }
-        } else {
-            if (!is_writable($project_path) || !is_readable($project_path)) {
-                die("No Read/Write Permission");
-            }
-        }
-    }
-    $project_data = array("name"=>$project_name,"path"=>$project_path);
+	if (!isAbsPath($project_path)) {
+		$project_path = str_replace(" ", "_", preg_replace('/[^\w-\.]/', '', $project_path));
+		mkdir($workspace . "/" . $project_path);
+	} else {
+		$project_path = cleanPath($project_path);
+		if (substr($project_path, -1) == '/') {
+			$project_path = substr($project_path, 0, strlen($project_path)-1);
+		}
+		if (!file_exists($project_path)) {
+			if (!mkdir($project_path.'/', 0755, true)) {
+				die("Unable to create Absolute Path");
+			}
+		} else {
+			if (!is_writable($project_path) || !is_readable($project_path)) {
+				die("No Read/Write Permission");
+			}
+		}
+	}
+	$project_data = array("name" => $project_name, "path" => $project_path);
 
-    saveJSON($projects, array($project_data));
+	saveJSON($projects, array($project_data));
 
-    //////////////////////////////////////////////////////////////////
-    // Create Users file
-    //////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////
+	// Create Users file
+	//////////////////////////////////////////////////////////////////
 
-    $user_data = array("username"=>$username,"password"=>$password,"project"=>$project_path);
+	$user_data = array(
+		"username" => $username,
+		"password" => $password,
+		"activeProject" => $project_path,
+		"permissions" => ["configure", "read", "write"],
+		"userACL" => "full"
+	);
 
-    saveJSON($users, array($user_data));
+	saveJSON($users, array($user_data));
 
-    //////////////////////////////////////////////////////////////////
-    // Create Active file
-    //////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////
+	// Create Active file
+	//////////////////////////////////////////////////////////////////
 
-    saveJSON($active, array(''));
-    
-    //////////////////////////////////////////////////////////////////
-    // Create Config
-    //////////////////////////////////////////////////////////////////
+	saveJSON($active, array(''));
+
+	//////////////////////////////////////////////////////////////////
+	// Create Config
+	//////////////////////////////////////////////////////////////////
 
 
-    $config_data = '<?php
+	$config_data = '<?php
 
 /*
 *  Copyright (c) Codiad & Kent Safranski (codiad.com), distributed
@@ -187,9 +182,9 @@ define("WSURL", BASE_URL . "/workspace");
 define("UPDATEURL", "http://https://atheos.io/update?v={VER}&o={OS}&p={PHP}&w={WEB}&a={ACT}");
 define("ARCHIVEURL", "https://github.com/Atheos/Atheos/archive/master.zip");
 define("COMMITURL", "https://api.github.com/repos/Atheos/Atheos/commits");
-';
+	';
 
-    saveFile($config, $config_data);
+	saveFile($config, $config_data);
 
-    echo("success");
+	echo("success");
 }
