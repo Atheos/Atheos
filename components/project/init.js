4a0519f8a29851ae71aa7983b48ce2ff24474593
Liam Siira <liam@siira.io>
2020-04-10 23:38:22 -0400

    Began working on fixing up the project component, trying to ensure it is functional to fix the permissions bug noted in one of the on going issues.
    Former-commit-id: c5b474badb49adf604087aba6b88662b7ce95d65

--- components/project/init.js
+++ components/project/init.js
@@ -1,12 +1,19 @@
-/*
-	*  Copyright (c) atheos & Kent Safranski (atheos.com), distributed
-	*  as-is and without warranty under the MIT License. See
-	*  [root]/license.txt for more. This information must remain intact.
-	*/
+/*jshint esversion: 6 */
+
+//////////////////////////////////////////////////////////////////////////////80
+// Project
+//////////////////////////////////////////////////////////////////////////////80
+// Copyright (c) Atheos & Liam Siira (Atheos.io), distributed as-is and without
+// warranty under the modified License: MIT - Hippocratic 1.2: firstdonoharm.dev
+// See [root]/license.md for more. This information must remain intact.
+//////////////////////////////////////////////////////////////////////////////80
+// Authors: Codiad Team, @Fluidbyte, Atheos Team, @hlsiira
+//////////////////////////////////////////////////////////////////////////////80
 
 (function(global, $) {
 
 	var atheos = global.atheos,
+		ajax = global.ajax,
 		amplify = global.amplify,
 		i18n = global.i18n,
 		oX = global.onyx;
@@ -14,7 +21,6 @@
 	var self = null;
 
 	amplify.subscribe('system.loadMinor', () => atheos.project.init());
-
 
 	atheos.project = {
 
@@ -173,88 +179,94 @@
 		// Load and list projects in the sidebar.
 		//////////////////////////////////////////////////////////////////
 		loadSide: function() {
-			// $('.sb-projects-content').load();
 			ajax({
 				url: this.dialog + '?action=sidelist&',
 				success: function(reply) {
 					oX('#project_list .content').html(reply);
-					// log(reply);
-					this.sideExpanded = true;
 				}
 			});
 		},
 
 		expand: function() {
 			this.sideExpanded = true;
-			$('#side-projects').css('height', 276 + 'px');
-			$('.project-list-title').css('right', 0);
-			$('.sb-left-content').css('bottom', 276 + 'px');
-			$('#projects-collapse')
-				.removeClass('icon-up-dir')
-				.addClass('icon-down-dir');
+			oX('#sb_left #project_list').css('height', '');
+			oX('#sb_left>.content').css('bottom', '');
+
+			oX('#projects-collapse').replaceClass('fa-chevron-circle-up', 'fa-chevron-circle-down');
 		},
 
 		collapse: function() {
 			this.sideExpanded = false;
-			$('#side-projects').css('height', 33 + 'px');
-			$('.project-list-title').css('right', 0);
-			$('.sb-left-content').css('bottom', 33 + 'px');
-			$('#projects-collapse')
-				.removeClass('icon-down-dir')
-				.addClass('icon-up-dir');
+			var height = oX('#sb_left #project_list .title').height();
+
+			oX('#sb_left #project_list').css('height', height + 'px');
+			oX('#sb_left>.content').css('bottom', height + 'px');
+
+			oX('#projects-collapse').replaceClass('fa-chevron-circle-down', 'fa-chevron-circle-up');
+
 		},
 
 		//////////////////////////////////////////////////////////////////
 		// Create Project
 		//////////////////////////////////////////////////////////////////
-
 		create: function(close) {
-			var _this = this;
-			atheos.modal.load(500, this.dialog + '?action=create&close=' + close);
-			$('#modal_content form')
-				.live('submit', function(e) {
-					e.preventDefault();
-					var projectName = $('#modal_content form input[name="project_name"]').val(),
-						projectPath = $('#modal_content form input[name="project_path"]').val(),
-						gitRepo = $('#modal_content form input[name="git_repo"]').val(),
-						gitBranch = $('#modal_content form input[name="git_branch"]').val();
-					var create = function() {
-						$.get(_this.controller + '?action=create&project_name=' + encodeURIComponent(projectName) + '&project_path=' + encodeURIComponent(projectPath) + '&git_repo=' + gitRepo + '&git_branch=' + gitBranch, function(data) {
-							var createdata = atheos.jsend.parse(data);
-							if (createdata !== 'error') {
-								_this.open(createdata.path);
-								atheos.modal.unload();
-								_this.loadSide();
-								/* Notify listeners. */
-								amplify.publish('project.onCreate', {
-									'name': projectName,
-									'path': projectPath,
-									'git_repo': gitRepo,
-									'git_branch': gitBranch
-								});
-							}
-						});
-					};
-					if (projectPath.indexOf('/') === 0) {
-						atheos.alert.show({
-							banner: 'Do you really want to create a project with an absolute path?',
-							data: projectPath,
-							positive: {
-								message: 'Yes',
-								fnc: function() {
-									create();
 
-								}
-							},
-							negative: {
-								message: 'No',
-								fnc: function() {}
-							}
-						});
-					} else {
-						create();
+			var projectName, projectPath, gitRepo, gitBranch;
+
+			var createProject = function() {
+				var data = {
+					action: 'create',
+					projectName,
+					projectPath,
+					gitRepo,
+					gitBranch
+				};
+
+				ajax({
+					url: self.controller,
+					data,
+					success: function(reply) {
+						if (reply.status !== 'error') {
+							self.open(reply.path);
+							self.loadSide();
+							/* Notify listeners. */
+							delete data.action;
+							amplify.publish('project.create', data);
+						}
 					}
 				});
+			};
+
+			var listener = function(e) {
+				e.preventDefault();
+
+				projectName = oX('#modal_content form input[name="project_name"]').value();
+				projectPath = oX('#modal_content form input[name="project_path"]').value();
+				gitRepo = oX('#modal_content form input[name="git_repo"]').value();
+				gitBranch = oX('#modal_content form input[name="git_branch"]').value();
+
+
+				if (projectPath.indexOf('/') === 0) {
+					atheos.alert.show({
+						banner: 'Do you really want to create a project with an absolute path?',
+						data: projectPath,
+						actions: {
+							'Yes': function() {
+								createProject();
+							},
+							'No': function() {}
+						}
+					});
+				} else {
+					createProject();
+				}
+			};
+
+			amplify.subscribe('modal.loaded', function() {
+				oX('#modal_content form').once('submit', listener);
+			});
+			atheos.modal.load(500, this.dialog + '?action=create&close=' + close);
+
 		},
 
 		//////////////////////////////////////////////////////////////////
@@ -262,31 +274,40 @@
 		//////////////////////////////////////////////////////////////////
 
 		rename: function(name, path) {
-			var _this = this;
 			atheos.modal.load(500, this.dialog + '?action=rename&path=' + encodeURIComponent(path) + '&name=' + name);
 
-			$('#modal_content form')
-				.live('submit', function(e) {
-					e.preventDefault();
-					var projectPath = $('#modal_content form input[name="project_path"]')
-						.val();
-					var projectName = $('#modal_content form input[name="project_name"]')
-						.val();
-					$.get(_this.controller + '?action=rename&project_path=' + encodeURIComponent(projectPath) + '&project_name=' + encodeURIComponent(projectName), function(data) {
-						var renamedata = atheos.jsend.parse(data);
-						if (renamedata !== 'error') {
+			var listener = function(e) {
+				e.preventDefault();
+
+				var projectPath = oX('#modal_content form input[name="project_path"]').value();
+				var projectName = oX('#modal_content form input[name="project_name"]').value();
+
+				var data = {
+					action: 'rename',
+					projectPath,
+					projectName
+				};
+
+				ajax({
+					url: self.controller,
+					data,
+					success: function(reply) {
+						if (reply.status !== 'error') {
 							atheos.toast.show('success', 'Project renamed');
-							_this.loadSide();
-							$('#file-manager a[data-type="root"]').html(projectName);
+							self.loadSide();
 							atheos.modal.unload();
 							/* Notify listeners. */
-							amplify.publish('project.onRename', {
-								'path': projectPath,
-								'name': projectName
-							});
+							delete data.action;
+							amplify.publish('project.rename', data);
 						}
-					});
+					}
 				});
+			};
+
+			amplify.subscribe('modal.loaded', function() {
+				oX('#modal_content form').once('submit', listener);
+			});
+			atheos.modal.load(500, this.dialog + '?action=create&close=' + close);
 		},
 
 		//////////////////////////////////////////////////////////////////
@@ -351,19 +372,18 @@
 		//////////////////////////////////////////////////////////////////
 
 		getCurrent: function() {
-			var project = this;
 			ajax({
-				url: this.controller,
+				url: self.controller,
 				data: {
 					action: 'current'
 				},
 				success: function(data) {
 					if (data.status === 'success') {
-						project.current.path = data.path;
+						self.current.path = data.path;
 					}
 				}
 			});
-			return project.current.path;
+			return self.current.path;
 		}
 	};
 })(this, jQuery);
