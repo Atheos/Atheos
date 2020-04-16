4b163e2d474821cf4194b185a047c957c822eb77
Liam Siira <liam@siira.io>
2020-04-15 21:52:38 -0400

    More house keeping of updating Settings (and more User) component to use the latest and greatest things.

--- components/settings/dialog.php
+++ components/settings/dialog.php
@@ -1,16 +1,20 @@
 <?php
 
-/*
-    *  Copyright (c) atheos & Andr3as, distributed
-    *  as-is and without warranty under the MIT License. See
-    *  [root]/license.txt for more. This information must remain intact.
-    */
+//////////////////////////////////////////////////////////////////////////////80
+// Settings Dialog
+//////////////////////////////////////////////////////////////////////////////80
+// Copyright (c) Atheos & Liam Siira (Atheos.io), distributed as-is and without
+// warranty under the modified License: MIT - Hippocratic 1.2: firstdonoharm.dev
+// See [root]/license.md for more. This information must remain intact.
+//////////////////////////////////////////////////////////////////////////////80
+// Authors: Codiad Team, @Fluidbyte, Atheos Team, @hlsiira
+//////////////////////////////////////////////////////////////////////////////80
 
 require_once("../../common.php");
 
-//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////80
 // Verify Session or Key
-//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////80
 checkSession();
 
 $action = Common::data("action");
@@ -21,6 +25,38 @@ if (!$action) {
 }
 
 switch ($action) {
+
+	//////////////////////////////////////////////////////////////////////////80
+	// Settings iFrame
+	//////////////////////////////////////////////////////////////////////////80
+	case "iframe":
+		?>
+		<script>
+			/*
+                 *  Storage Event:
+                 *  Note: Event fires only if change was made in different window and not in this one
+                 *  Details: http://dev.w3.org/html5/webstorage/#dom-localstorage
+                 */
+			window.addEventListener('storage',
+				function(e) {
+					if (/^atheos/.test(e.key)) {
+						var obj = {
+							key: e.key,
+							oldValue: e.oldValue,
+							newValue: e.newValue
+						};
+						/* Notify listeners */
+						window.parent.amplify.publish('settings.changed', obj);
+					}
+				},
+				false);
+		</script>
+		<?php
+		break;
+
+	//////////////////////////////////////////////////////////////////////////80
+	// Main Settings Window
+	//////////////////////////////////////////////////////////////////////////80
 	case "settings":
 		?>
 		<h1><i class="fas fa-cog"></i><?php i18n("Settings"); ?></h1>
@@ -71,32 +107,11 @@ switch ($action) {
 		</div>
 		<?php
 		break;
-	case "iframe":
-		?>
-		<script>
-			/*
-                 *  Storage Event:
-                 *  Note: Event fires only if change was made in different window and not in this one
-                 *  Details: http://dev.w3.org/html5/webstorage/#dom-localstorage
-                 */
-			window.addEventListener('storage',
-				function(e) {
-					if (/^atheos/.test(e.key)) {
-						var obj = {
-							key: e.key,
-							oldValue: e.oldValue,
-							newValue: e.newValue
-						};
-						/* Notify listeners */
-						window.parent.amplify.publish('settings.changed', obj);
-					}
-				},
-				false);
-		</script>
-		<?php
-		break;
+
+	//////////////////////////////////////////////////////////////////////////80
+	// Default: Invalid Action
+	//////////////////////////////////////////////////////////////////////////80
 	default:
 		Common::sendJSON("E401i");
 		break;
 }
-?>
