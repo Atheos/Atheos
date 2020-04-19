5e7b2dd7343ba887930e4960fb85a1f1a313746a
Liam Siira <liam@siira.io>
2020-04-19 16:29:48 -0400

    Removed the upload/download feature of the filemanager into their own component. Made it a lot easier to fix and update those functions when I didn\'t have to be worried about breaking the file manager.

---
+++ components/transfer/download.php
@@ -0,0 +1,40 @@
+<?php
+
+//////////////////////////////////////////////////////////////////////////////80
+// Transfer Download
+//////////////////////////////////////////////////////////////////////////////80
+// Copyright (c) Atheos & Liam Siira (Atheos.io), distributed as-is and without
+// warranty under the modified License: MIT - Hippocratic 1.2: firstdonoharm.dev
+// See [root]/license.md for more. This information must remain intact.
+//////////////////////////////////////////////////////////////////////////////80
+// Authors: Codiad Team, @Fluidbyte, Atheos Team, @hlsiira
+//////////////////////////////////////////////////////////////////////////////80
+
+require_once('../../common.php');
+
+//////////////////////////////////////////////////////////////////////////////80
+// Verify Session or Key
+//////////////////////////////////////////////////////////////////////////////80
+Common::checkSession();
+
+$filename = Common::data("filename");
+Common::cleanPath($filename);
+
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename="'.basename($filename).'"');
+header('Content-Transfer-Encoding: binary');
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Content-Length: ' . filesize($filename));
+
+if (ob_get_contents()) {
+	ob_end_clean();
+}
+
+flush();
+
+readfile($filename);
+// Remove temp file
+unlink($filename);
