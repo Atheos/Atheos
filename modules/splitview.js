43b589261411d4307f95cbd78314721ce5a17027
Liam Siira <liam@siira.io>
2020-04-28 15:25:02 -0400

    Partial finished rewrite of the SplitView module. Pretty happy with it as it is currently, read more in my notes within the module.
    Former-commit-id: 15c918c669b32248bf4fd088bc3846c15e5741ba

--- modules/splitview.js
+++ modules/splitview.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 
 //////////////////////////////////////////////////////////////////////////////80
-// SplitView Class
+// SplitView Module
 //////////////////////////////////////////////////////////////////////////////80
 // Copyright (c) Atheos & Liam Siira (Atheos.io), distributed as-is and without
 // warranty under the modified License: MIT - Hippocratic 1.2: firstdonoharm.dev
@@ -9,20 +9,8 @@
 //////////////////////////////////////////////////////////////////////////////80
 // Authors: Codiad Team, @Fluidbyte, Atheos Team, @hlsiira
 //////////////////////////////////////////////////////////////////////////////80
-// Notes: 
-// The context menu should become an object stored within the filemanager, and
-// constructed based on the fules specified therein. The OBJ is created, and then
-// added to by each plugin based on it's requirements. The OBJ could even be 
-// cached.
-//												- Liam Siira
-//////////////////////////////////////////////////////////////////////////////80
-
 
 (function(global) {
-
-	//////////////////////////////////////////////////////////////////////////80
-	// Collection of wrapper functions for depreciated calls.
-	//////////////////////////////////////////////////////////////////////////80
 
 	var atheos = global.atheos,
 		amplify = global.amplify,
@@ -30,15 +18,11 @@
 
 	amplify.subscribe('system.loadExtra', () => atheos.splitview.init());
 
-	var self = null;
-
 	atheos.splitview = {
 
 		splitMenuOpen: false,
 
 		init: function() {
-			self = this;
-
 			atheos.common.initMenuHandler(oX('#split'), oX('#split-options-menu'));
 
 			oX('#split-vertically').on('click', function(e) {
@@ -61,161 +45,136 @@
 
 var separatorWidth = 5;
 
-function SplitContainer(root, children, splitType) {
+
+//////////////////////////////////////////////////////////////////////////////80
+// SplitView Class
+//////////////////////////////////////////////////////////////////////////////80
+// Notes:
+// Getting rid of self and moving all the functions inside of Split Container
+// might clean up the code a little more.
+//
+// I've thought about pushing the mini CSS library into a global scope to make
+// the code a little cleaner as well, but am worried that someone might try to 
+// use it instead of Onyx, which while it's not a problem to do so, it goes
+// against what I consider best practices. I might add it to Onyx in a sort of
+// high performance little brother type deal for Onyx later.
+//
+// The cMajor, cMinor, and Borders are are held miniCSS objects, while the
+// Parent is still a raw dom element. It added a bit of a headache to try to
+// turn it into a handler so I left it as it is.
+//
+// The performance side of the SplitContainer I consider to be pretty much done
+// with little left to improve as far as I can tell, the readability leaves a
+// lot to desire. I tried to do a lot of CodeGolf with this one in order to
+// challenge myself a little bit and will probably use this module as a sandbox
+// for non-best practices / CodeGolf.
+//
+// There is room for improvement I'm pretty sure in using Right/Bottom as well
+// as Left/Top for each child in that you'd no longer have to set those values
+// after you initially did so, only having to change all the widths. Or even 
+// switching the magical FlexBox container, but I'm not super familiar with
+// FlexBox, and rewriting this to use Right/Bottom seems like a very low 
+// priority to me right now. I'm happy where SplitContainer is currently.
+//												- Liam Siira
+//////////////////////////////////////////////////////////////////////////////80
+
+function SplitContainer(parent, children, splitType) {
 	var self = this,
 		css = self.css;
 
-	self.root = root;
+	self.parent = parent;
 	self.splitType = splitType;
-	self.childContainers = {};
-	self.childElements = {};
 	self.splitProp = 0.5;
 
-	var cMajor = self.cMajor = children[0];
-	var cMinor = self.cMinor = children[1];
-	var border = self.border = document.createElement('div');
-	var rHeight = css(self.root).height();
-	var rWidth = css(self.root).width();
-
-	self.setChild(0, self.cMajor);
-	self.setChild(1, self.cMinor);
-
+	self.border = css(document.createElement('div'));
 
 	if (splitType === 'horizontal') {
-		border.classList.add('splitter', 'h-splitter');
-		css(border).width(separatorWidth);
-		css(border).height(rHeight);
+		self.border.dom.classList.add('splitter', 'h-splitter');
+		self.border.width(separatorWidth);
+		self.border.height(css(self.parent).height());
 
 	} else if (splitType === 'vertical') {
-		border.classList.add('splitter', 'v-splitter');
-		css(border).height(separatorWidth);
-		css(border).width(rWidth);
+		self.border.dom.classList.add('splitter', 'v-splitter');
+		self.border.height(separatorWidth);
+		self.border.width(css(self.parent).width());
 	}
 
-	root.append(border);
+	self.parent.append(self.border.dom);
+	self.setChild(0, children[0]);
+	self.setChild(1, children[1]);
+
 
 	// You might be tempted in removing the arrow function here, but it would 
 	// break scope on this/self down in the Drag function.
-	css(border).on('mousedown', (e) => self.drag(e));
+	self.border.on('mousedown', (e) => self.drag(e));
+	css(self.parent).on('h-resize', (e) => self.resize(e, 'width'));
+	css(self.parent).on('v-resize', (e) => self.resize(e, 'height'));
 
-	css(self.root).on('h-resize', function(e) {
-		// log('self.root');
-		log(self.splitType);
-
-		if (self.splitType === 'horizontal') {
-			var w1 = rWidth * self.splitProp - separatorWidth / 2;
-			var w2 = rWidth * (1 - self.splitProp) - separatorWidth / 2;
-
-			css(cMajor).width(w1);
-			css(cMinor).width(w2);
-			css(cMinor).left(w1 + separatorWidth);
-			// Border top might already be set by the drag.
-			css(border).left(w1);
-
-
-		} else if (self.splitType === 'vertical') {
-			css(cMajor).width(rWidth);
-			css(cMinor).width(rWidth);
-			css(border).width(rWidth);
-		}
-
-		css(cMajor).trigger('v-resize');
-		css(cMinor).trigger('h-resize');
-
-	});
-
-	css(self.root).on('v-resize', function(e) {
-		rHeight = css(self.root).height();
-
-		if (self.splitType === 'horizontal') {
-			css(cMajor).height(rHeight);
-			css(cMinor).height(rHeight);
-			css(border).height(rHeight);
-
-		} else if (self.splitType === 'vertical') {
-			var h1 = rHeight * self.splitProp - separatorWidth / 2;
-			var h2 = rHeight * (1 - self.splitProp) - separatorWidth / 2;
-
-			css(cMajor).height(h1);
-			css(cMinor).height(h2);
-			css(cMinor).top(h1 + separatorWidth);
-			// Border top might already be set by the drag.
-			css(border).top(h1);
-		}
-
-		css(cMajor).trigger('v-resize');
-		css(cMinor).trigger('v-resize');
-	});
-
-	css(self.root).trigger('h-resize');
-	css(self.root).trigger('v-resize');
+	css(self.parent).trigger('h-resize');
+	css(self.parent).trigger('v-resize');
 }
 
 SplitContainer.prototype = {
 	setChild: function(splitID, element) {
-		if (element instanceof SplitContainer) {
-			if (splitID === 0) {
-				this.cMajor = element.root;
-			} else {
-				this.cMinor = element.root;
-			}
-			trace(splitID);
-			log(element);
-			log(splitID);
-
-			element.splitID = splitID;
-			element = element.root;
-			// this.childContainers[splitID] = element;
-		}
-
-		this.root.append(element);
-		this.cssInit(splitID, element);
-
-	},
-	cssInit: function(splitID, element) {
 		var self = this,
 			css = self.css;
-		var h1, h2, w1, w2;
 
-		var rHeight = css(self.root).height();
-		var rWidth = css(self.root).width();
-
-		if (self.splitType === 'horizontal') {
-
-			w1 = rWidth * self.splitProp - (separatorWidth / 2);
-			w2 = rWidth * (1 - self.splitProp) - (separatorWidth / 2);
-
+		if (element instanceof SplitContainer) {
 			if (splitID === 0) {
-				css(element).top(0);
-				css(element).left(0);
-				css(element).width(w1);
-				css(element).height(rHeight);
+				self.cMajor = css(element.parent);
 			} else {
-				css(element).top(0);
-				css(element).left(w1 + separatorWidth);
-				css(element).width(w2);
-				css(element).height(rHeight);
+				self.cMinor = css(element.parent);
 			}
-
-		} else if (self.splitType === 'vertical') {
-
-			h1 = rHeight * self.splitProp - (separatorWidth / 2);
-			h2 = rHeight * (1 - self.splitProp) - (separatorWidth / 2);
-
+			element.splitID = splitID;
+			element = element.parent;
+		} else {
 			if (splitID === 0) {
-				css(element).top(0);
-				css(element).left(0);
-				css(element).width(h1);
-				css(element).height(rWidth);
+				self.cMajor = css(element);
 			} else {
-				css(element).top(h1 + separatorWidth);
-				css(element).left(0);
-				css(element).width(h2);
-				css(element).height(rWidth);
+				self.cMinor = css(element);
 			}
-
 		}
+
+		self.parent.append(element);
+
+		css(element).top(0);
+		css(element).left(0);
+		css(self.parent).trigger('h-resize');
+		css(self.parent).trigger('v-resize');
 	},
+
+	resize: function(event, type) {
+		var self = this,
+			css = self.css;
+
+		var s0, s1, s2;
+
+		s0 = css(self.parent)[type]();
+		s1 = s0 * self.splitProp - separatorWidth / 2;
+		s2 = s0 * (1 - self.splitProp) - separatorWidth / 2;
+
+		if (self.splitType === 'horizontal' && type === 'width') {
+			self.cMajor.width(s1);
+			self.cMinor.width(s2);
+			self.cMinor.left(s1 + separatorWidth);
+			self.border.left(s1);
+
+		} else if (self.splitType === 'vertical' && type === 'height') {
+			self.cMajor.height(s1);
+			self.cMinor.height(s2);
+			self.cMinor.top(s1 + separatorWidth);
+			self.border.top(s1);
+
+		} else {
+			self.cMajor[type](s0);
+			self.cMinor[type](s0);
+			self.border[type](s0);
+		}
+
+		self.cMajor.trigger(event.type);
+		self.cMinor.trigger(event.type);
+	},
+
 	drag: function(event) {
 		//References: http://jsfiddle.net/8wtq17L8/ & https://jsfiddle.net/tovic/Xcb8d/
 		var self = this,
@@ -227,51 +186,32 @@ SplitContainer.prototype = {
 			splitX = border.offsetLeft,
 			splitY = border.offsetTop;
 
-		var rHeight = css(self.root).height();
-		var rWidth = css(self.root).width();
+		var rHeight = css(self.parent).height();
+		var rWidth = css(self.parent).width();
 		var timeout = false;
 
-		function effectChange(top, left) {
-			timeout = false;
-			if (self.splitType === 'horizontal') {
-
-				// var w1 = left;
-				// var w2 = rWidth - left;
-
-				self.splitProp = left / rWidth;
-
-				// css(self.cMajor).width(w1);
-				// css(self.cMinor).width(w2);
-				// css(self.cMinor).left(w1 + separatorWidth);
-
-				css(self.root).trigger('h-resize');
-				// css(self.cMinor).trigger('h-resize');
-
-			} else {
-
-				// var h1 = top - separatorWidth / 2;
-				// var h2 = rWidth - top - separatorWidth / 2;
-				self.splitProp = top / rHeight;
-
-				// css(self.cMajor).height(h1);
-				// css(self.cMinor).height(h2);
-				// css(self.cMinor).top(h1 + separatorWidth);
-
-				css(self.root).trigger('v-resize');
-				// css(self.cMinor).trigger('v-resize');
-
-			}
-		}
-
-		function moveElement(event) {
+		// Set the throttle as a named variable in order to add AND remove
+		// the event listener later on. Without it being a named item, it can't
+		// be removed.
+		var moveElement = zed.throttle(function(event) {
 			var left = splitX + event.clientX - mouseX,
 				top = splitY + event.clientY - mouseY;
+
 			border.style.left = self.splitType === 'horizontal' ? left + 'px' : '';
 			border.style.top = self.splitType === 'vertical' ? top + 'px' : '';
-			if (timeout === false) {
-				timeout = setTimeout(() => effectChange(top, left), 10);
+
+			if (self.splitType === 'horizontal') {
+				var percent = left / rWidth;
+				percent = percent < 0.1 ? 0.1 : percent;
+				percent = percent > 0.9 ? 0.9 : percent;
+
+				self.splitProp = percent;
+				css(self.parent).trigger('h-resize');
+			} else {
+				self.splitProp = top / rHeight;
+				css(self.parent).trigger('v-resize');
 			}
-		}
+		}, 10);
 
 		function disableSelect(event) {
 			event.preventDefault();
@@ -284,7 +224,6 @@ SplitContainer.prototype = {
 			window.removeEventListener('selectstart', disableSelect);
 		}
 
-		// document.onmousemove = _move_elem;
 		document.addEventListener('mousemove', moveElement, false);
 		document.addEventListener('mouseup', removeListeners, false);
 		window.addEventListener('selectstart', disableSelect);
@@ -328,6 +267,18 @@ SplitContainer.prototype = {
 			}));
 		};
 
+		let updateListener = (type, fnc, change) => {
+			type = type.split(',');
+			type.forEach((t) => {
+				t = t.trim();
+				if (change === 'add') {
+					element.addEventListener(t, fnc);
+				} else {
+					element.removeEventListener(t, fnc);
+				}
+			})
+		};
+
 		return {
 			top: (v) => element.style.top = v + 'px',
 			left: (v) => element.style.left = v + 'px',
@@ -335,10 +286,10 @@ SplitContainer.prototype = {
 			bottom: (v) => element.style.bottom = v + 'px',
 			width: (v) => setSize('width', v),
 			height: (v) => setSize('height', v),
-			on: (t, f) => element.addEventListener(t, f),
-			off: (t, f) => element.removeEventListener(t, f),
-			trigger: (t) => triggerEvent(t)
+			on: (t, f) => updateListener(t, f, 'add'),
+			off: (t, f) => updateListener(t, f, 'remove'),
+			trigger: (t) => triggerEvent(t),
+			dom: element
 		};
 	}
-
 };
