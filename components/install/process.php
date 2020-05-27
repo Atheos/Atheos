503998a334518458f5713e668931e22dfc5f5b00
Liam Siira <liam@siira.us>
2020-05-27 08:55:13 -0400

    Added a development global variable that\'s off by default. Nw, if you are developer, you can easily disable/enable the server side minification process, and I don\'t have to worry about enabling it before a release.

--- components/install/process.php
+++ components/install/process.php
@@ -175,8 +175,11 @@ $cookie_lifetime = "0";
 // TIMEZONE
 date_default_timezone_set("' . $timezone . '");
 
-// External Authentification
-//define("AUTH_PATH", "/path/to/customauth.php");
+// DEVELOPMENT MODE
+define("DEVELOPMENT", false);
+
+// EXTERNAL AUTHENTICATION
+// define("AUTH_PATH", "/path/to/customauth.php");
 
 //////////////////////////////////////////////////////////////////////////////80
 // ** DO NOT EDIT CONFIG BELOW **
