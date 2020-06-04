4710c90a046c85dc15e212c9b43b54c520e42909
Liam Siira <liam@siira.us>
2020-06-03 22:16:32 -0400

    Added a keybindings info table to the settings menu

---
+++ components/settings/settings.keybindings.php
@@ -0,0 +1,84 @@
+<?php require_once('../../common.php'); ?>
+
+<label><i class="fas fa-keyboard"></i><?php i18n("Keybindings"); ?></label>
+<table class="keybindings">
+	<tr>
+		<td>
+			<?php i18n("Close Modal"); ?>
+		</td>
+		<td>
+			<i class="cmd">Esc</i>
+		</td>
+
+		<td>
+			<?php i18n("Open CodeGit"); ?>
+		</td>
+		<td>
+			<i class="cmd">Ctrl</i><i class="fas fa-plus"></i><i class="key">C</i>
+		</td>
+	</tr>
+
+	<tr>
+		<td>
+			<?php i18n("Save Active File"); ?>
+		</td>
+		<td>
+			<i class="cmd">Ctrl</i><i class="fas fa-plus"></i><i class="key">S</i>
+		</td>
+
+		<td>
+			<?php i18n("Close Active File"); ?>
+		</td>
+		<td>
+			<i class="cmd">Ctrl</i><i class="fas fa-plus"></i><i class="key">W</i>
+		</td>
+	</tr>
+
+	<tr>
+		<td>
+			<?php i18n("Find"); ?>
+		</td>
+		<td>
+			<i class="cmd">Ctrl</i><i class="fas fa-plus"></i><i class="key">F</i>
+		</td>
+
+		<td>
+			<?php i18n("Open Scout"); ?>
+		</td>
+		<td>
+			<i class="cmd">Ctrl</i><i class="fas fa-plus"></i><i class="key">E</i>
+		</td>
+	</tr>
+
+	<tr>
+		<td>
+			<?php i18n("Replace"); ?>
+		</td>
+		<td>
+			<i class="cmd">Ctrl</i><i class="fas fa-plus"></i><i class="key">R</i>
+		</td>
+
+		<td>
+			<?php i18n("GoTo Line"); ?>
+		</td>
+		<td>
+			<i class="cmd">Ctrl</i><i class="fas fa-plus"></i><i class="key">L</i>
+		</td>
+	</tr>
+
+	<tr>
+		<td>
+			<?php i18n("Cycle to Previous File"); ?>
+		</td>
+		<td>
+			<i class="cmd">Ctrl</i><i class="fas fa-plus"></i><i class="fas fa-arrow-alt-circle-up"></i>
+		</td>
+
+		<td>
+			<?php i18n("Cycle to Next File"); ?>
+		</td>
+		<td>
+			<i class="cmd">Ctrl</i><i class="fas fa-plus"></i><i class="fas fa-arrow-alt-circle-down"></i>
+		</td>
+	</tr>
+</table>
