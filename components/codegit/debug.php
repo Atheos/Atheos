685c56ff4666a097508c595d19d01dc36c45a17f
Liam Siira <liam@siira.io>
2020-06-05 19:37:06 -0400

    Moved these into a proper archive folder

--- components/codegit/debug.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// require_once('IGit.php');
-require_once('GitRepository.php');
-
-// create repo object
-$repo = new Cz\Git\GitRepository('../Atheos-Ignore');
-
-echo json_encode($repo->getChanges());
