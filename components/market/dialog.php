fb4c3056f7054982d1575fa848b7881b249aa9ed
Liam Siira <liam@siira.io>
2020-06-15 00:42:54 -0400

    Updated the restricted methods for opening dialogs

--- components/market/dialog.php
+++ components/market/dialog.php
@@ -24,12 +24,6 @@ if (!$action) {
 	die;
 }
 
-if (!Common::checkAccess("configure")) {
-	Common::sendJSON("E430u");
-	die;
-}
-
-
 $Market = new Market();
 
 
@@ -39,49 +33,60 @@ switch ($action) {
 	// Marketplace
 	//////////////////////////////////////////////////////////////////////////80
 	case 'list':
-		
-		$table = $Market->renderMarket();
-		?>
-		<label class="title"><i class="fas fa-store"></i><?php echo i18n("atheosMarketplace"); ?></label>
-		<div id="market">
-			<!--<table width="100%">-->
-			<!--	<tr>-->
-			<!--<th valign="middle" align="center" width="40px">-->
-			<!--	<button onclick="window.location.reload();return false;"><?php echo i18n("reloadAtheos"); ?></button>-->
-			<!--</th>-->
-			<!--		<th valign="middle">-->
-			<!--			<input type="text" id="manual_repo" placeholder="<?php echo i18n("enterGitHubRepositoryURL"); ?>">-->
-			<!--			<button class="btn-left" id="manual_install"><?php echo i18n("market_install_manually"); ?></button>-->
-			<!--		</th>-->
-			<!--	</tr>-->
-			<!--</table>-->
-			<table id="market_table" width="100%">
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Description</th>
-						<th>Author</th>
-						<th>Actions</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td colspan="4">
-							<h2>Installed</h2>
-						</td>
-					</tr>
-					<?php echo $table["i"]; ?>
-					<tr>
-						<td colspan="4">
-							<h2>Available</h2>
-						</td>
-					</tr>
-					<?php echo $table["a"]; ?>
-				</tbody>
 
-			</table>
-		</div>
-		<?php
+
+		if (!Common::checkAccess("configure")) {
+			?>
+			<label class="title"><i class="fas fa-sync"></i><?php echo i18n("restricted"); ?></label>
+			<form>
+				<label class="title"><i class="fas fa-sync"></i><?php echo i18n("restricted_marketplace"); ?></label>
+			</form>
+			<?php
+		} else {
+
+			$table = $Market->renderMarket();
+			?>
+			<label class="title"><i class="fas fa-store"></i><?php echo i18n("atheosMarketplace"); ?></label>
+			<div id="market">
+				<!--<table width="100%">-->
+				<!--	<tr>-->
+				<!--<th valign="middle" align="center" width="40px">-->
+				<!--	<button onclick="window.location.reload();return false;"><?php echo i18n("reloadAtheos"); ?></button>-->
+				<!--</th>-->
+				<!--		<th valign="middle">-->
+				<!--			<input type="text" id="manual_repo" placeholder="<?php echo i18n("enterGitHubRepositoryURL"); ?>">-->
+				<!--			<button class="btn-left" id="manual_install"><?php echo i18n("market_install_manually"); ?></button>-->
+				<!--		</th>-->
+				<!--	</tr>-->
+				<!--</table>-->
+				<table id="market_table" width="100%">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Description</th>
+							<th>Author</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td colspan="4">
+								<h2>Installed</h2>
+							</td>
+						</tr>
+						<?php echo $table["i"]; ?>
+						<tr>
+							<td colspan="4">
+								<h2>Available</h2>
+							</td>
+						</tr>
+						<?php echo $table["a"]; ?>
+					</tbody>
+
+				</table>
+			</div>
+			<?php
+		}
 		break;
 	default:
 		Common::sendJSON("E401i");
