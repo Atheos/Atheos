4cbe1d3e59ae926d04008aa088f0541f2cb46cc1
Liam Siira <liam@siira.io>
2020-06-27 18:16:09 -0400

    Started routing all traffic through a single controller file. It would allow me to cut out all requests to common from every dialog and controller, and cut out a ton of code repetition as well as pushing more of the traffic between the client and server behind an HTTP post instead of the url.

---
+++ controller.php
@@ -0,0 +1,33 @@
+<?php
+
+//////////////////////////////////////////////////////////////////////////////80
+// Atheos Controller
+//////////////////////////////////////////////////////////////////////////////80
+// Copyright (c) Atheos & Liam Siira (Atheos.io), distributed as-is and without
+// warranty under the modified License: MIT - Hippocratic 1.2: firstdonoharm.dev
+// See [root]/license.md for more. This information must remain intact.
+//////////////////////////////////////////////////////////////////////////////80
+// Authors: Atheos Team, @hlsiira
+//////////////////////////////////////////////////////////////////////////////80
+
+require_once('common.php');
+
+//////////////////////////////////////////////////////////////////////////////80
+// Verify Session or Key
+//////////////////////////////////////////////////////////////////////////////80
+Common::checkSession();
+
+$action = Common::data("action");
+$target = Common::data("target");
+
+if (!$action || !$target) {
+	Common::sendJSON("E401m"); die;
+}
+
+if (file_exists("components/$target/controller.php")) {
+	require("components/$target/controller.php");
+} elseif (file_exists("plugins/$target/controller.php")) {
+	require("plugins/$target/controller.php");
+} else {
+	Common::sendJSON("E401m"); die;
+}
