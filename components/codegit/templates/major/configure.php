2ec893f31563c187b0b92bb4f1e34ac04907f359
Liam Siira <liam@siira.io>
2020-07-19 22:21:49 -0400

    Catching some errors

--- components/codegit/templates/major/configure.php
+++ components/codegit/templates/major/configure.php
@@ -1,5 +1,11 @@
 <?php
 $settings = $CodeGit->settings($repo);
+
+$global_name = isset($settings["global"]["name"]) && $settings["global"]["name"] ? $settings["global"]["name"][0] : "";
+$global_email = isset($settings["global"]["email"]) && $settings["global"]["email"] ? $settings["global"]["email"][0] : "";
+$local_name = isset($settings["local"]["name"]) && $settings["local"]["name"] ? $settings["local"]["name"][0] : "";
+$local_email = isset($settings["local"]["email"]) && $settings["local"]["email"] ? $settings["local"]["email"][0] : "";
+
 ?>
 
 <div id="codegit_configure" class="content">
@@ -11,9 +17,9 @@ $settings = $CodeGit->settings($repo);
 				<fieldset id="global">
 					<legend>Global settings (WIP)</legend>
 					<label for="global_name">Name</label>
-					<input type="text" name="global_name" id="global_name" placeholder="<?php echo $settings["global"]["name"][0] ?>" disabled>
+					<input type="text" name="global_name" id="global_name" placeholder="<?php echo $global_name ?>" disabled>
 					<label for="global_email">E-Mail</label>
-					<input type="text" name="global_email" id="global_email" placeholder="<?php echo $settings["global"]["email"][0] ?>" disabled>
+					<input type="text" name="global_email" id="global_email" placeholder="<?php echo $global_email ?>" disabled>
 					<button onclick="atheos.codegit.configure('global');"  disabled><?php echo i18n("update") ?></button>
 				</fieldset>
 			</td>
@@ -21,9 +27,9 @@ $settings = $CodeGit->settings($repo);
 				<fieldset id="local">
 					<legend>Local settings for <?php echo $repo ?></legend>
 					<label for="local_name">Name</label>
-					<input type="text" name="local_name" id="local_name" placeholder="<?php echo $settings["local"]["name"][0] ?>">
+					<input type="text" name="local_name" id="local_name" placeholder="<?php echo $local_email ?>">
 					<label for="local_email">E-Mail</label>
-					<input type="text" name="local_email" id="local_email" placeholder="<?php echo $settings["local"]["email"][0] ?>">
+					<input type="text" name="local_email" id="local_email" placeholder="<?php echo $local_name ?>">
 					<button onclick="atheos.codegit.configure('local');"><?php echo i18n("update") ?></button>
 
 				</fieldset>
