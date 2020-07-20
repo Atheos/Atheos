e435c21b77976294ce4f23755d1a98f37e55ddc9
Liam Siira <liam@siira.io>
2020-07-19 22:09:15 -0400

    Added ability to configure git user on a per repo basis

--- components/codegit/templates/major/configure.php
+++ components/codegit/templates/major/configure.php
@@ -1,27 +1,34 @@
 <?php
-$localRepo = false; // basename `git rev-parse --show-toplevel`
-
 $settings = $CodeGit->settings($repo);
-
 ?>
 
-
-<label class="title"><i class="fas fa-git-branch"></i><?php echo i18n("configure"); ?></label>
-
 <div id="codegit_configure" class="content">
-	<fieldset id="remote">
-		<legend>Global<span> - Global for all repositories</span></legend>
-		<label for="global_username">Username</label>
-		<input type="text" name="global_username" id="global_username" placeholder="<?php echo $settings["name"]?>">
-		<label for="global_email">E-Mail</label>
-		<input type="text" name="global_email" id="global_email" placeholder="<?php echo $settings["email"]?>">
-	</fieldset>
+	<label class="title"><i class="fas fa-user-cog"></i><?php echo i18n("configure"); ?></label>
 
-	<!--<fieldset id="local">-->
-	<!--	<legend>Local<span> - for <?php echo $localRepo ?></span></legend>-->
-	<!--	<label for="local_username">Username</label>-->
-	<!--	<input type="text" name="local_username" id="local_username" class="local">-->
-	<!--	<label for="local_email">E-Mail</label>-->
-	<!--	<input type="text" name="local_email" id="local_email" class="local">-->
-	<!--</fieldset>-->
+	<table>
+		<tr>
+			<td>
+				<fieldset id="global">
+					<legend>Global settings (WIP)</legend>
+					<label for="global_name">Name</label>
+					<input type="text" name="global_name" id="global_name" placeholder="<?php echo $settings["global"]["name"][0] ?>" disabled>
+					<label for="global_email">E-Mail</label>
+					<input type="text" name="global_email" id="global_email" placeholder="<?php echo $settings["global"]["email"][0] ?>" disabled>
+					<button onclick="atheos.codegit.configure('global');"  disabled><?php echo i18n("update") ?></button>
+				</fieldset>
+			</td>
+			<td>
+				<fieldset id="local">
+					<legend>Local settings for <?php echo $repo ?></legend>
+					<label for="local_name">Name</label>
+					<input type="text" name="local_name" id="local_name" placeholder="<?php echo $settings["local"]["name"][0] ?>">
+					<label for="local_email">E-Mail</label>
+					<input type="text" name="local_email" id="local_email" placeholder="<?php echo $settings["local"]["email"][0] ?>">
+					<button onclick="atheos.codegit.configure('local');"><?php echo i18n("update") ?></button>
+
+				</fieldset>
+			</td>
+		</tr>
+	</table>
 </div>
+<hint>These settings are pulled from Git</hint>
