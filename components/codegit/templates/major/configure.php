8c155aea244a83553d3526518cf5682f3a9238f3
Liam Siira <liam@siira.io>
2020-07-20 21:27:29 -0400

    Completed the user configuration system

--- components/codegit/templates/major/configure.php
+++ components/codegit/templates/major/configure.php
@@ -1,40 +1,38 @@
 <?php
 $settings = $CodeGit->settings($repo);
 
-$global_name = isset($settings["global"]["name"]) && $settings["global"]["name"] ? $settings["global"]["name"][0] : "";
-$global_email = isset($settings["global"]["email"]) && $settings["global"]["email"] ? $settings["global"]["email"][0] : "";
-$local_name = isset($settings["local"]["name"]) && $settings["local"]["name"] ? $settings["local"]["name"][0] : "";
-$local_email = isset($settings["local"]["email"]) && $settings["local"]["email"] ? $settings["local"]["email"][0] : "";
-
+$global_name = isset($settings["global"]["name"]) ? $settings["global"]["name"] : "";
+$global_email = isset($settings["global"]["email"]) ? $settings["global"]["email"] : "";
+$local_name = isset($settings[$repo]["name"]) ? $settings[$repo]["name"] : "";
+$local_email = isset($settings[$repo]["email"]) ? $settings[$repo]["email"] : "";
 ?>
 
 <div id="codegit_configure" class="content">
 	<label class="title"><i class="fas fa-user-cog"></i><?php echo i18n("configure"); ?></label>
-
 	<table>
 		<tr>
 			<td>
 				<fieldset id="global">
-					<legend>Global settings (WIP)</legend>
-					<label for="global_name">Name</label>
-					<input type="text" name="global_name" id="global_name" placeholder="<?php echo $global_name ?>" disabled>
-					<label for="global_email">E-Mail</label>
-					<input type="text" name="global_email" id="global_email" placeholder="<?php echo $global_email ?>" disabled>
-					<button onclick="atheos.codegit.configure('global');"  disabled><?php echo i18n("update") ?></button>
+					<legend><?php echo i18n("git_settings_global")?></legend>
+					<label for="global_name"><?php echo i18n("name")?></label>
+					<input type="text" name="global_name" id="global_name" placeholder="<?php echo $global_name ?>">
+					<label for="global_email"><?php echo i18n("email")?></label>
+					<input type="text" name="global_email" id="global_email" placeholder="<?php echo $global_email ?>">
+					<button onclick="atheos.codegit.configure('global');"><?php echo i18n("update") ?></button>
+					<button onclick="atheos.codegit.configure('clear_global');"><?php echo i18n("clear") ?></button>
 				</fieldset>
 			</td>
 			<td>
 				<fieldset id="local">
-					<legend>Local settings for <?php echo $repo ?></legend>
-					<label for="local_name">Name</label>
-					<input type="text" name="local_name" id="local_name" placeholder="<?php echo $local_email ?>">
-					<label for="local_email">E-Mail</label>
-					<input type="text" name="local_email" id="local_email" placeholder="<?php echo $local_name ?>">
+					<legend><?php echo i18n("git_settings_local", $repo)?></legend>
+					<label for="local_name"><?php echo i18n("name")?></label>
+					<input type="text" name="local_name" id="local_name" placeholder="<?php echo $local_name ?>">
+					<label for="local_email"><?php echo i18n("email")?></label>
+					<input type="text" name="local_email" id="local_email" placeholder="<?php echo $local_email ?>">
 					<button onclick="atheos.codegit.configure('local');"><?php echo i18n("update") ?></button>
-
+					<button onclick="atheos.codegit.configure('clear_local');"><?php echo i18n("clear") ?></button>
 				</fieldset>
 			</td>
 		</tr>
 	</table>
 </div>
-<hint>These settings are pulled from Git</hint>
