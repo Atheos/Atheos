5775c2d8c96f7652c97698a216f4ea2c640520a0
Liam Siira <liam@siira.io>
2020-09-06 22:59:26 -0400

    Cleaned up the update component, most of it has been moved into the market or new analytics component
    Former-commit-id: ec0e79c638ea010d8346119744d41cb984f1ca6d

--- components/update/dialog.php
+++ components/update/dialog.php
@@ -29,11 +29,9 @@ switch ($action) {
 			</form>
 			<?php
 		} else {
-			require_once('class.update.php');
-			$local = $Update->local;
-			$remote = $Update->remote;
+			$latest = $Update->latest;
 
-			$body = preg_replace('/\*\*/i', "", $remote["body"]);
+			$body = preg_replace('/\*\*/i', "", $latest["body"]);
 			$body = str_replace("Changes:", "", $body);
 
 			?>
@@ -44,16 +42,14 @@ switch ($action) {
 				<input type="hidden" name="remoteversion" value="">
 				<br>
 				<table>
-					<tr><td width="40%"><?php echo i18n("yourVersion"); ?></td><td><?php echo ucfirst($local['atheos_version']); ?></td></tr>
-					<tr><td width="40%"><?php echo i18n("latestVersion"); ?></td><td><?php echo $remote['tag_name']; ?></td></tr>
+					<tr><td width="40%"><?php echo i18n("yourVersion"); ?></td><td><?php echo ucfirst(VERSION); ?></td></tr>
+					<tr><td width="40%"><?php echo i18n("latestVersion"); ?></td><td><?php echo $latest['tag_name']; ?></td></tr>
 				</table>
 				<br>
 				<label><?php echo i18n("update_changes"); ?></label>
 				<pre id="update_changes"><?php echo $body; ?></pre>
-				<?php if ($local['atheos_version'] === "nightly") {
-					?>
-					<hint><?php echo i18n("nightly"); ?></hint>
-					<?php
+				<?php if (DEVELOPMENT) {
+					echo("<hint>" . i18n("nightly") . "</hint>");
 				} ?>
 				<br>
 				<toolbar>
