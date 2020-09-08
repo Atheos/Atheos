82fc65b3ddd321601df15c80ea4b599c25f55758
Liam Siira <liam@siira.io>
2020-09-07 20:20:32 -0400

    Changed quotes
    Former-commit-id: f56511ecdc055f2e12adf588f6414cca3e70ce13

--- components/analytics/dialog.php
+++ components/analytics/dialog.php
@@ -17,28 +17,28 @@ switch ($action) {
 	//////////////////////////////////////////////////////////////////////////80
 	// Update
 	//////////////////////////////////////////////////////////////////////////80
-	case 'check':
+	case "check":
 
 		if (!Common::checkAccess("configure")) {
 			echo("<h1>" . i18n("restricted") . "</h1>");
 			echo("<pre>" . i18n("youCanNotCheckForUpdates") . "</pre>");
 		} else {
-			require_once('class.update.php');
+			require_once("class.update.php");
 			$update = new Update();
 			$vars = json_decode($update->check(), true);
-			$local = $vars['local'];
+			$local = $vars["local"];
 			?>
 			<label class="title"><i class="fas fa-sync"></i><?php echo i18n("updateCheck"); ?></label>
 			<form>
 				<input type="hidden" name="archive" value="">
 				<input type="hidden" name="remoteversion" value="">
 				<br><table>
-					<tr><td width="40%"><?php echo i18n("yourVersion"); ?></td><td><?php echo $local['atheos_version']; ?></td></tr>
+					<tr><td width="40%"><?php echo i18n("yourVersion"); ?></td><td><?php echo $local["atheos_version"]; ?></td></tr>
 					<tr><td width="40%"><?php echo i18n("latestVersion"); ?></td><td id="remote_latest"></td></tr>
 				</table>
 				<br><label><?php echo i18n("changesOnAtheos"); ?></label>
 				<pre id="update_changes"></pre>
-				<?php if ($local['atheos_version'] === "nightly") {
+				<?php if ($local["atheos_version"] === "nightly") {
 					?>
 					<br><em class="note"><?php echo i18n("noteYourInstallationIsANightlyBuildCodiadMightBeUnstable"); ?></em><br>
 					<?php
