11351d1d6f7ad0c71415498acacc2da6f21334b8
Liam Siira <liam@siira.io>
2021-02-26 20:37:08 +0000

    Merged documentation into source file for easier maintainability and cleaned up the module a little bit

--- modules/keybind.js
+++ modules/keybind.js
@@ -6,30 +6,52 @@
 // This information must remain intact.
 //////////////////////////////////////////////////////////////////////////////80
 // Description:
-// Keybinding module for adding keyboard shortcuts to functions. Exposes init()
-// and bind() to global for use by plugins.
-//												- Liam Siira
+// Keybinding module for adding keyboard shortcuts to functions. Exposes bind()
+// and rebind() to global for use by plugins.
+//////////////////////////////////////////////////////////////////////////////80
+// Suggestions:
+//	Better customization of modifer keys
+//////////////////////////////////////////////////////////////////////////////80
+// Usage:
+//  - ctrl: Ctrl key is also pressed (CMD on Macs)
+//  -  alt: Alt key is also pressed
+//
+//	atheos.keybind.add(87, 'ctrl', function(text) {
+//		console.log("This will log when Ctrl + W is pressed:" + text);
+//	}, ['Custom Message']);
 //////////////////////////////////////////////////////////////////////////////80
 
-(function(global) {
+(function() {
 
-	var atheos = global.atheos,
-		carbon = global.carbon;
-
-	var self = null;
+	let self = false;
 
 	carbon.subscribe('system.loadMinor', () => atheos.keybind.init());
 
-
-	//////////////////////////////////////////////////////////////////////
-	// Bindings
-	//////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////80
+	// Default Bindings
+	//////////////////////////////////////////////////////////////////////////80////////////////////////////////////
+	// HotKey  | Function                                | HotKey             | Function                           |
+	// ------- | --------------------------------------- | ------------------ | ---------------------------------- |
+	// ESC     | Close any open dialogs / Exit multiedit | CTRL+Space bar     | Autocomplete                       |
+	// CTRL+S  | Save current file                       | ALT+UP             | Move current line(s) up            |
+	// CTRL+O  | Open current file in browser            | ALT+DOWN           | Move current line(s) down          |
+	// CTRL+F  | Find in current editor                  | ALT+Shift+UP       | Copy current line(s) above current |
+	// CTRL+R  | Find & Replace/All in current editor    | ALT+Shift+DOWN     | Copy current line(s) below current |
+	// CTRL+H  | Advanced Search / Search & Replace      | CTRL+UP            | Switch to previous tab             |
+	// CTRL+A  | Select All                              | CTRL+DOWN          | Switch to next tab                 |
+	// CTRL+D  | Delete current line(s)                  | CTRL+Z             | Undo                               |
+	// CTRL+P  | Find matching element [{()}]            | CTRL+Y             | Redo                               |
+	// CTRL+L  | Go To Line                              | ALT+0              | Fold all                           |
+	// CTRL+\[ | Decrease current line(s) indent         | ALT+Shift+0        | Unfold all                         |
+	// CTRL+]  | Increase current line(s) indent         | CTRL+/             | Comment Line                       |
+	//////////////////////////////////////////////////////////////////////////80////////////////////////////////////
 
 	atheos.keybind = {
 
 		bindings: {},
 
 		init: function() {
+			if (self) return;
 			self = this;
 
 			document.addEventListener('keydown', self.handler);
@@ -77,27 +99,27 @@
 				atheos.active.move('down');
 			});
 
-			// Merge Editor Vertically [CTRL+M] ////////////////////////80
+			// Merge Editor Vertically [CTRL+M] //////////////////////////////80
 			self.bind(77, 'ctrl', function() {
-				var activeSession = atheos.editor.activeInstance.getSession();
+				let activeSession = atheos.editor.getSession();
 				atheos.editor.exterminate();
 				atheos.editor.addInstance(activeSession);
 			});
 
 			// Split Editor Horizonally [CTRL+;] /////////////////////////////80
 			self.bind(186, 'ctrl', function() {
-				atheos.editor.addInstance(atheos.editor.activeInstance.getSession(), 'right');
+				atheos.editor.addInstance(atheos.editor.getSession(), 'right');
 			});
 
 			// Split Editor Vertically [CTRL+Shift+;] ////////////////////////80
 			self.bind(186, 'alt', function() {
-				atheos.editor.addInstance(atheos.editor.activeInstance.getSession(), 'bottom');
+				atheos.editor.addInstance(atheos.editor.getSession(), 'bottom');
 			});
 		},
 
-		//////////////////////////////////////////////////////////////////////
+		//////////////////////////////////////////////////////////////////////80
 		// Key Bind
-		//////////////////////////////////////////////////////////////////////
+		//////////////////////////////////////////////////////////////////////80
 		bind: function(key, cmd, callback, args) {
 			if (!(key in self.bindings)) {
 				self.bindings[key] = [];
@@ -110,6 +132,9 @@
 			});
 		},
 
+		//////////////////////////////////////////////////////////////////////80
+		// Key Rebind
+		//////////////////////////////////////////////////////////////////////80
 		rebind: function(oldKey, newKey) {
 			if (oldKey in self.bindings) {
 				self.bindings[newKey] = self.bindings[oldKey];
@@ -117,26 +142,23 @@
 			}
 		},
 
+		//////////////////////////////////////////////////////////////////////80
+		// Event Handler
+		//////////////////////////////////////////////////////////////////////80
 		handler: function(e) {
-			if (e.keyCode in self.bindings) {
-				self.bindings[e.keyCode].forEach(function(bind) {
-					if (bind.cmd.includes('alt') !== e.altKey) {
-						return;
-					} else if (bind.cmd.includes('ctrl') !== (e.ctrlKey || e.metaKey)) {
-						return;
-					} else if (bind.cmd.includes('shift') !== e.shiftKey) {
-						return;
-					}
+			if (!(e.keyCode in self.bindings)) return;
 
-					e.preventDefault();
-					e.stopPropagation();
+			self.bindings[e.keyCode].forEach(function(bind) {
+				if (bind.cmd.includes('alt') !== e.altKey) return;
+				if (bind.cmd.includes('ctrl') !== (e.ctrlKey || e.metaKey)) return;
+				if (bind.cmd.includes('shift') !== e.shiftKey) return;
 
-					bind.callback.apply(this, bind.args);
-				});
+				e.preventDefault();
+				e.stopPropagation();
 
-
-			}
+				bind.callback.apply(this, bind.args);
+			});
 		}
 	};
 
-})(this);
+})();
