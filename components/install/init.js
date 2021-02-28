b18861c4b4e78243aaf0e770aa8dfd0be1dc29b2
Liam Siira <liam@siira.io>
2021-02-28 18:37:14 +0000

    Added workspace definition
    Former-commit-id: 5270d29b18590898b7aa0cd0ac73e5127c38169b

--- components/install/init.js
+++ components/install/init.js
@@ -72,6 +72,7 @@
 				url: 'components/install/process.php',
 				data,
 				settled: function(status, reply) {
+					log(status,reply);
 					if (status === 'success') {
 						setTimeout(() => window.location.reload(), 100);
 						toast(status, reply);
