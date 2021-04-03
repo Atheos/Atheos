c3ccc455890cc76d7c318110c5a5f4ab9072c8ca
Liam Siira <liam@siira.io>
2021-04-03 03:37:13 +0000

    Moved stand alone libraries into a separate folder to keep a better distinction between Atheos modules and helper libraries

--- public/class.sourcemanager.php
+++ public/class.sourcemanager.php
@@ -11,18 +11,9 @@ use MatthiasMullie\Minify;
 class SourceManager {
 
 	private $modules = array(
-		"modules/carbon.js",
-		"modules/echo.js",
-		"modules/flux.js",
-		"modules/icons.js",
-		"modules/global.js",
-		"modules/onyx.js",
-		"modules/synthetic.js",
-		// Global components need to be above this line.
 		"modules/system.js",
 		"modules/alert.js",
 		"modules/flow.js",
-		"modules/flux.js",
 		"modules/chrono.js",
 		"modules/common.js",
 		"modules/i18n.js",
