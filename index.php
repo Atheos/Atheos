fc62cfe42f61f378d0e4403b1046f3aa8cd9d0c2
Liam Siira <liam@siira.io>
2021-04-03 04:43:24 +0000

    Added an error document, modified the html pages to use templates for common code
    Former-commit-id: 37bfef8fa3d9a454f10aeb8a961cd842a95b793d

--- index.php
+++ index.php
@@ -32,20 +32,13 @@ $SourceManager = new SourceManager;
 <head>
 	<meta charset="utf-8">
 	<title>Atheos IDE</title>
+	<meta name="author" content="Liam Siira">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="A Web-Based IDE with a small footprint and minimal requirements">
 
 	<!-- FAVICONS -->
-	<link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png?v=2">
-	<link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png?v=2">
-	<link rel="manifest" href="favicons/site.webmanifest">
-	<link rel="mask-icon" href="favicons/safari-pinned-tab.svg?v=2" color="#5bbad5">
-	<link rel="shortcut icon" href="favicons/favicon.ico?v=2">
-	<meta name="msapplication-TileColor" content="#111111">
-	<meta name="msapplication-config" content="favicons/browserconfig.xml">
-	<meta name="theme-color" content="#ffffff">
-
 	<?php
+	require_once("templates/favicons.php");
 
 	// Load THEME
 	echo("<!-- THEME -->\n");
---
+++ templates/logo.php
@@ -0,0 +1,25 @@
+<div id="logo">
+	<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="40 147.5 320 387.5" width="320" height="335">
+		<defs>
+			<path id="backdrop" class="delay-0" d="M200.42 460.36L65.67 384.02V231.34L200.42 155l134.75 76.34v152.68l-134.75 76.34z" />
+			<path id="leftWing" class="delay-3" d="M40 400V220l80 40v100l60 35v85L40 400z" />
+			<path id="leftBar" class="delay-3" d="M120 360l-80 40" />
+			<path id="rightWing" class="delay-3" d="M360 400V220l-80 40v100l-60 35v85l140-80z" />
+			<path id="rightBar" class="delay-3" d="M280 360l80 40" />
+			<path id="alpha" class="delay-2" d="M255 355l-55-115-55 115" />
+			<path id="center" class="delay-3" d="M200 490l20-10v-85l60-35V260l-80-50-80 50v100l60 35v85l20 10z" />
+			<clipPath id="i" class="delay-2">
+				<use xlink:href="#alpha" />
+			</clipPath>
+		</defs>
+		<use xlink:href="#backdrop" fill="var(--navy)" />
+		<use xlink:href="#leftWing" fill-opacity="0" stroke="var(--blue)" stroke-width="15" />
+		<use xlink:href="#leftBar" fill-opacity="0" stroke="var(--blue)" stroke-width="15" />
+		<use xlink:href="#rightWing" fill-opacity="0" stroke="var(--blue)" stroke-width="15" />
+		<use xlink:href="#rightBar" fill-opacity="0" stroke="var(--blue)" stroke-width="15" />
+		<use xlink:href="#center" fill-opacity="0" stroke="var(--blue)" stroke-width="15" />
+		<g clip-path="url(#i)">
+			<use  xlink:href="#alpha" fill-opacity="0" stroke="var(--shade0)" stroke-width="30" />
+		</g>
+	</svg>
+</div>
