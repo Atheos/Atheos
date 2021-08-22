7ff61ddb65e8fcf93aa73c8e0d6c388506a4dc75
Liam Siira <liam@siira.io>
2021-08-22 13:11:56 -0400

    Small formatting changes to Synthetic, cleaning it up a touch

--- modules/system.js
+++ modules/system.js
@@ -47,7 +47,7 @@
 				atheos.settings.init();
 
 			} else {
-				synthetic.init();
+				Synthetic.init();
 				if (oX('#installer').exists()) {
 					// Atheos hasn't been installed yet
 					atheos.install.init();
