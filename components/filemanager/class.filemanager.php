e16ea804cb13755e8c1cd004ec9b73dc4014837b
Liam Siira <liam@siira.io>
2022-05-06 17:05:06 -0400

    Fixes #227, issue with sub folder installations not being recognized on url preview

--- components/filemanager/class.filemanager.php
+++ components/filemanager/class.filemanager.php
@@ -244,10 +244,10 @@ class Filemanager {
 		$files = array();
 		foreach ($index as $item => $data) {
 			$link = false;
-			if(is_link($data["path"])) {
+			if (is_link($data["path"])) {
 				$link = readlink($data["path"]);
 			}
-			
+
 			if ($data["type"] === "folder") {
 
 				$repo = file_exists($data["path"] . "/.git");
@@ -334,8 +334,9 @@ class Filemanager {
 			$prot = "http://";
 		}
 		$domain = SERVER("HTTP_HOST");
-		$url = rtrim($prot . $domain . "/workspace/" . Common::getRelativePath($path), "/");
-
+		$page = pathinfo(SERVER("REQUEST_URI"), PATHINFO_DIRNAME);
+		$url = rtrim($prot . $domain . $page . "/workspace/" . Common::getRelativePath($path), "/");
+		$url = str_replace("//workspace", "/workspace", $url);
 		Common::send("success", $url);
 	}
 
