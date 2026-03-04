1d9a5f97b5c21f48bfe2acb10ab0ae5a5cdf44dc
Liam Siira <liam@siira.io>
2026-03-04 14:21:54 -0500

    Fixes #328 by encoding high level unicode during patch process. Root cause appears to be that PHP and JS count Unicode characters differently (1 vs 2), such that patch indices are offset. Best solution would be to fix diff_match_patch to explicitly handle Unicode characters consistently, but I couldn't figure out how. Scouring other repos for insight didn't help either; problem requires a lot more time and coding talent than I have.

--- components/worker/worker.js
+++ components/worker/worker.js
@@ -1,18 +1,49 @@
 importScripts('../../vendor/differential/diff_match_patch.min.js');
 
+
+function encodeHighUnicode(str) {
+    const pattern  = /[\uD800-\uDBFF][\uDC00-\uDFFF]/g;
+    const callback = (match) => {
+        const codepoint = match.codePointAt(0);
+        const paddedHex = codepoint.toString(16).padStart(6, '0');
+        return '\uE000' + paddedHex + '\uE001';
+    };
+
+    return str.replace(pattern, callback);
+}
+
+function decodeHighUnicode(str) {
+    const pattern  = /\uE000([0-9a-f]{6})\uE001/g;
+    const callback = (match, hex) => {
+        const codepoint = parseInt(hex, 16);
+        return String.fromCodePoint(codepoint);
+    };
+
+    return str.replace(pattern, callback);
+}
+
+
 var tasks = {
-    diff: function(config){
-        var dmp = new diff_match_patch();
-        var patchTxt = dmp.patch_toText(dmp.patch_make(config.original, config.changed));
-        return {
-            success: true,
-            result: patchTxt
-        };
-    }
+	diff: function(config) {
+		var dmp = new diff_match_patch();
+		
+		// Encode all high unicode characters before patching.
+		// This fixes differences in encoding between PHP and JS.
+        var original = encodeHighUnicode(config.original);
+        var changed  = encodeHighUnicode(config.changed);
+		// var patches = dmp.patch_make(config.original, config.changed);
+		var patches = dmp.patch_make(original, changed);
+		var patchTxt = dmp.patch_toText(patches);
+
+		return {
+			success: true,
+			result: patchTxt
+		};
+	}
 };
 
-self.addEventListener('message', function(e){
-    var config = e.data;
-    var outcome = tasks[config.taskType](config);
-    self.postMessage(outcome);
+self.addEventListener('message', function(e) {
+	var config = e.data;
+	var outcome = tasks[config.taskType](config);
+	self.postMessage(outcome);
 }, false);
