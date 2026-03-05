674b51a6fbf977cea85ccc05f048b083d3a50fca
Xoriander <simon_h7@gmx.net>
2026-03-05 17:40:49 +0100

    Avoids an exception on first login loadState call through recursive folder creation Without the recursive creation of the data/users/username folder, open a file on first login after install leads into an internal server error

--- traits/database.php
+++ traits/database.php
@@ -31,7 +31,7 @@ class Store {
     protected $data = array();
 
     function __construct($table, $path) {
-        if (!is_dir($path)) mkdir($path);
+        if (!is_dir($path)) mkdir($path, 0755, true);
 
         $this->path = $path . $table . ".db.php";
         if (is_readable($this->path)) {
